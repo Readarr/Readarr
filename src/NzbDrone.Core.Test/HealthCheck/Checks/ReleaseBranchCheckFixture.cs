6c3bc1acb7f4a4407ff49f38836ca52b84c22935
Qstick <qstick@gmail.com>
2020-02-28 20:56:37 -0500

    New: HealthCheck for valid Branch value
    (cherry picked from commit ef2f954b814b10094de25d691812e699a587e0f4)

---
+++ src/NzbDrone.Core.Test/HealthCheck/Checks/ReleaseBranchCheckFixture.cs
@@ -0,0 +1,51 @@
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Localization;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class ReleaseBranchCheckFixture : CoreTest<ReleaseBranchCheck>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mocker.GetMock<ILocalizationService>()
+                  .Setup(s => s.GetLocalizedString(It.IsAny<string>()))
+                  .Returns("Some Warning Message");
+        }
+
+        private void GivenValidBranch(string branch)
+        {
+            Mocker.GetMock<IConfigFileProvider>()
+                    .SetupGet(s => s.Branch)
+                    .Returns(branch);
+        }
+
+        [TestCase("book-index")]
+        [TestCase("phantom")]
+
+        // ToDo: Master should be valid once released
+        [TestCase("master")]
+        public void should_return_warning_when_branch_is_not_valid(string branch)
+        {
+            GivenValidBranch(branch);
+
+            Subject.Check().ShouldBeWarning();
+        }
+
+        [TestCase("nightly")]
+        [TestCase("Nightly")]
+        [TestCase("develop")]
+        [TestCase("Develop")]
+        public void should_return_no_warning_when_branch_valid(string branch)
+        {
+            GivenValidBranch(branch);
+
+            Subject.Check().ShouldBeOk();
+        }
+    }
+}
---
+++ src/NzbDrone.Core/HealthCheck/Checks/ReleaseBranchCheck.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Configuration.Events;
+using NzbDrone.Core.Localization;
+
+namespace NzbDrone.Core.HealthCheck.Checks
+{
+    [CheckOn(typeof(ConfigSavedEvent))]
+    public class ReleaseBranchCheck : HealthCheckBase
+    {
+        private readonly IConfigFileProvider _configFileService;
+
+        public ReleaseBranchCheck(IConfigFileProvider configFileService, ILocalizationService localizationService)
+            : base(localizationService)
+        {
+            _configFileService = configFileService;
+        }
+
+        public override HealthCheck Check()
+        {
+            var currentBranch = _configFileService.Branch.ToLower();
+
+            if (!Enum.GetNames(typeof(ReleaseBranches)).Any(x => x.ToLower() == currentBranch))
+            {
+                return new HealthCheck(GetType(), HealthCheckResult.Warning, string.Format(_localizationService.GetLocalizedString("ReleaseBranchCheckOfficialBranchMessage"), _configFileService.Branch), "#branch-is-not-a-valid-release-branch");
+            }
+
+            return new HealthCheck(GetType());
+        }
+
+        public enum ReleaseBranches
+        {
+            // ToDo Enable Master as valid once released
+            //Master,
+            Develop,
+            Nightly
+        }
+    }
+}
