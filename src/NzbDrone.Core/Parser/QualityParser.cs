ea471941812fdfd1c2ffe79acadad7613b366a15
ta264 <ta264@users.noreply.github.com>
2020-04-17 22:31:26 +0100

    Fixed: Better quality parser

--- src/NzbDrone.Core/Parser/QualityParser.cs
+++ src/NzbDrone.Core/Parser/QualityParser.cs
@@ -25,22 +25,7 @@ namespace NzbDrone.Core.Parser
         private static readonly Regex RealRegex = new Regex(@"\b(?<real>REAL)\b",
                                                                 RegexOptions.Compiled);
 
-        private static readonly Regex CodecRegex = new Regex(@"\b(?:(?<MP1>MPEG Version \d(.5)? Audio, Layer 1|MP1)|
-(?<MP2>MPEG Version \d(.5)? Audio, Layer 2|MP2)|
-(?<MP3VBR>MP3.*VBR|MPEG Version \d(.5)? Audio, Layer 3 vbr)|
-(?<MP3CBR>MP3|MPEG Version \d(.5)? Audio, Layer 3)|
-(?<FLAC>flac)|
-(?<WAVPACK>wavpack|wv)|
-(?<ALAC>alac)|
-(?<WMA>WMA\d?)|
-(?<WAV>WAV|PCM)|
-(?<AAC>M4A|M4P|M4B|AAC|mp4a|MPEG-4 Audio(?!.*alac))|
-(?<OGG>OGG|OGA|Vorbis))\b|
-(?<APE>monkey's audio|[\[|\(].*\bape\b.*[\]|\)])|
-(?<OPUS>Opus Version \d(.5)? Audio|[\[|\(].*\bopus\b.*[\]|\)])|
-(?<MOBI>\bMOBI\b)|
-(?<EPUB>\bEPUB\b)|
-(?<AZW3>\bAZW3?\b)",
+        private static readonly Regex CodecRegex = new Regex(@"\b(?:(?<MOBI>MOBI)|(?<EPUB>EPUB)|(?<AZW3>AZW3?)|(?<MP1>MPEG Version \d(.5)? Audio, Layer 1|MP1)|(?<MP2>MPEG Version \d(.5)? Audio, Layer 2|MP2)|(?<MP3VBR>MP3.*VBR|MPEG Version \d(.5)? Audio, Layer 3 vbr)|(?<MP3CBR>MP3|MPEG Version \d(.5)? Audio, Layer 3)|(?<FLAC>flac)|(?<WAVPACK>wavpack|wv)|(?<ALAC>alac)|(?<WMA>WMA\d?)|(?<WAV>WAV|PCM)|(?<AAC>M4A|M4P|M4B|AAC|mp4a|MPEG-4 Audio(?!.*alac))|(?<OGG>OGG|OGA|Vorbis))\b|(?<APE>monkey's audio|[\[|\(].*\bape\b.*[\]|\)])|(?<OPUS>Opus Version \d(.5)? Audio|[\[|\(].*\bopus\b.*[\]|\)])",
                                                              RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public static QualityModel ParseQuality(string name, string desc = null)
