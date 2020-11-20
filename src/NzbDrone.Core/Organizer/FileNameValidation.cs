ed0722bae4c3cdc8054cbb778a6a3043337149e2
Mark McDowall <mark@mcdowall.ca>
2020-11-20 14:43:04 -0800

    New: Validate that naming formats don't contain illegal characters
    Fixes #620
    
    (cherry picked from commit 145c644c9d8f1636027da8a782a7e74f3182c678)

--- src/NzbDrone.Core/Organizer/FileNameValidation.cs
+++ src/NzbDrone.Core/Organizer/FileNameValidation.cs
@@ -1,6 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation;
 using FluentValidation.Validators;
+using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Core.Organizer
 {
@@ -12,12 +16,16 @@ namespace NzbDrone.Core.Organizer
         public static IRuleBuilderOptions<T, string> ValidBookFormat<T>(this IRuleBuilder<T, string> ruleBuilder)
         {
             ruleBuilder.SetValidator(new NotEmptyValidator(null));
+            ruleBuilder.SetValidator(new IllegalCharactersValidator());
+
             return ruleBuilder.SetValidator(new ValidStandardTrackFormatValidator());
         }
 
         public static IRuleBuilderOptions<T, string> ValidAuthorFolderFormat<T>(this IRuleBuilder<T, string> ruleBuilder)
         {
             ruleBuilder.SetValidator(new NotEmptyValidator(null));
+            ruleBuilder.SetValidator(new IllegalCharactersValidator());
+            
             return ruleBuilder.SetValidator(new RegularExpressionValidator(FileNameBuilder.AuthorNameRegex)).WithMessage("Must contain Author name");
         }
     }
@@ -36,6 +44,44 @@ namespace NzbDrone.Core.Organizer
             if (!(FileNameBuilder.BookTitleRegex.IsMatch(value) && FileNameBuilder.PartRegex.IsMatch(value)) &&
                 !FileNameValidation.OriginalTokenRegex.IsMatch(value))
             {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class IllegalCharactersValidator : PropertyValidator
+    {
+        private readonly char[] _invalidPathChars = Path.GetInvalidPathChars();
+
+        public IllegalCharactersValidator()
+            : base("Contains illegal characters: {InvalidCharacters}")
+        {
+
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var value = context.PropertyValue as string;
+            var invalidCharacters = new List<char>();
+
+            if (value.IsNullOrWhiteSpace())
+            {
+                return true;
+            }
+
+            foreach (var i in _invalidPathChars)
+            {
+                if (value.IndexOf(i) >= 0)
+                {
+                    invalidCharacters.Add(i);
+                }
+            }
+
+            if (invalidCharacters.Any())
+            {
+                context.MessageFormatter.AppendArgument("InvalidCharacters", string.Join("", invalidCharacters));
                 return false;
             }
 
