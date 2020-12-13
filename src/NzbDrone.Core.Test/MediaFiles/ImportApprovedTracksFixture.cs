Lidarr/Lidarr

Pull DownloadClientItemClientInfo part of 824d315a

src/NzbDrone.Core.Test/MediaFiles/ImportApprovedTracksFixture.cs
@@ -27,6 +27,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         private List<ImportDecision<LocalTrack>> _approvedDecisions;
 
         private DownloadClientItem _downloadClientItem;
+        private DownloadClientItemClientInfo _clientInfo;
 
         [SetUp]
         public void Setup()
@@ -79,7 +80,8 @@ namespace NzbDrone.Core.Test.MediaFiles
                   .Setup(s => s.UpgradeTrackFile(It.IsAny<TrackFile>(), It.IsAny<LocalTrack>(), It.IsAny<bool>()))
                   .Returns(new TrackFileMoveResult());
 
-            _downloadClientItem = Builder<DownloadClientItem>.CreateNew().Build();
+            _clientInfo = Builder<DownloadClientItemClientInfo>.CreateNew().Build();
+            _downloadClientItem = Builder<DownloadClientItem>.CreateNew().With(x => x.DownloadClientInfo = _clientInfo).Build();
 
             Mocker.GetMock<IMediaFileService>()
                 .Setup(s => s.GetFilesByAlbum(It.IsAny<int>()))
@@ -187,7 +189,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         [Test]
         public void should_copy_when_cannot_move_files_downloads()
         {
-            Subject.Import(new List<ImportDecision<LocalTrack>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false });
+            Subject.Import(new List<ImportDecision<LocalTrack>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false, DownloadClientInfo = _clientInfo });
 
             Mocker.GetMock<IUpgradeMediaFiles>()
                   .Verify(v => v.UpgradeTrackFile(It.IsAny<TrackFile>(), _approvedDecisions.First().Item, true), Times.Once());
@@ -196,7 +198,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         [Test]
         public void should_use_override_importmode()
         {
-            Subject.Import(new List<ImportDecision<LocalTrack>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false }, ImportMode.Move);
+            Subject.Import(new List<ImportDecision<LocalTrack>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false, DownloadClientInfo = _clientInfo }, ImportMode.Move);
 
             Mocker.GetMock<IUpgradeMediaFiles>()
                   .Verify(v => v.UpgradeTrackFile(It.IsAny<TrackFile>(), _approvedDecisions.First().Item, false), Times.Once());

src/NzbDrone.Core/Download/IgnoredDownloadService.cs
@@ -35,15 +35,15 @@ namespace NzbDrone.Core.Download
             }
 
             var downloadIgnoredEvent = new DownloadIgnoredEvent
-                                      {
-                                          ArtistId = artist.Id,
-                                          AlbumIds = albums.Select(e => e.Id).ToList(),
-                                          Quality = trackedDownload.RemoteAlbum.ParsedAlbumInfo.Quality,
-                                          SourceTitle = trackedDownload.DownloadItem.Title,
-                                          DownloadClient = trackedDownload.DownloadItem.DownloadClient,
-                                          DownloadId = trackedDownload.DownloadItem.DownloadId,
-                                          Message = "Manually ignored"
-                                      };
+            {
+                ArtistId = artist.Id,
+                AlbumIds = albums.Select(e => e.Id).ToList(),
+                Quality = trackedDownload.RemoteAlbum.ParsedAlbumInfo.Quality,
+                SourceTitle = trackedDownload.DownloadItem.Title,
+                DownloadClientInfo = trackedDownload.DownloadItem.DownloadClientInfo,
+                DownloadId = trackedDownload.DownloadItem.DownloadId,
+                Message = "Manually ignored"
+            };
 
             _eventAggregator.PublishEvent(downloadIgnoredEvent);
             return true;

Readarr/Readarr

src/NzbDrone.Core.Test/MediaFiles/ImportApprovedTracksFixture.cs
@@ -28,6 +28,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         private List<ImportDecision<LocalBook>> _approvedDecisions;
 
         private DownloadClientItem _downloadClientItem;
+        private DownloadClientItemClientInfo _clientInfo;
 
         [SetUp]
         public void Setup()
@@ -74,7 +75,8 @@ namespace NzbDrone.Core.Test.MediaFiles
                   .Setup(s => s.UpgradeBookFile(It.IsAny<BookFile>(), It.IsAny<LocalBook>(), It.IsAny<bool>()))
                   .Returns(new BookFileMoveResult());
 
-            _downloadClientItem = Builder<DownloadClientItem>.CreateNew().Build();
+            _clientInfo = Builder<DownloadClientItemClientInfo>.CreateNew().Build();
+            _downloadClientItem = Builder<DownloadClientItem>.CreateNew().With(x => x.DownloadClientInfo = _clientInfo).Build();
 
             Mocker.GetMock<IMediaFileService>()
                 .Setup(s => s.GetFilesByBook(It.IsAny<int>()))
@@ -186,7 +188,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         [Test]
         public void should_copy_when_cannot_move_files_downloads()
         {
-            Subject.Import(new List<ImportDecision<LocalBook>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false });
+            Subject.Import(new List<ImportDecision<LocalBook>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false, DownloadClientInfo = _clientInfo });
 
             Mocker.GetMock<IUpgradeMediaFiles>()
                   .Verify(v => v.UpgradeBookFile(It.IsAny<BookFile>(), _approvedDecisions.First().Item, true), Times.Once());
@@ -195,7 +197,7 @@ namespace NzbDrone.Core.Test.MediaFiles
         [Test]
         public void should_use_override_importmode()
         {
-            Subject.Import(new List<ImportDecision<LocalBook>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false }, ImportMode.Move);
+            Subject.Import(new List<ImportDecision<LocalBook>> { _approvedDecisions.First() }, true, new DownloadClientItem { Title = "Alien.Ant.Farm-Truant", CanMoveFiles = false, DownloadClientInfo = _clientInfo }, ImportMode.Move);
 
             Mocker.GetMock<IUpgradeMediaFiles>()
                   .Verify(v => v.UpgradeBookFile(It.IsAny<BookFile>(), _approvedDecisions.First().Item, false), Times.Once());

src/NzbDrone.Core/Download/IgnoredDownloadService.cs
@@ -35,15 +35,15 @@ namespace NzbDrone.Core.Download
             }
 
             var downloadIgnoredEvent = new DownloadIgnoredEvent
-                                      {
-                                          AuthorId = author.Id,
-                                          BookIds = books.Select(e => e.Id).ToList(),
-                                          Quality = trackedDownload.RemoteBook.ParsedBookInfo.Quality,
-                                          SourceTitle = trackedDownload.DownloadItem.Title,
-                                          DownloadClient = trackedDownload.DownloadItem.DownloadClient,
-                                          DownloadId = trackedDownload.DownloadItem.DownloadId,
-                                          Message = "Manually ignored"
-                                      };
+            {
+                AuthorId = author.Id,
+                BookIds = books.Select(e => e.Id).ToList(),
+                Quality = trackedDownload.RemoteBook.ParsedBookInfo.Quality,
+                SourceTitle = trackedDownload.DownloadItem.Title,
+                DownloadClientInfo = trackedDownload.DownloadItem.DownloadClientInfo,
+                DownloadId = trackedDownload.DownloadItem.DownloadId,
+                Message = "Manually ignored"
+            };
 
             _eventAggregator.PublishEvent(downloadIgnoredEvent);
             return true;