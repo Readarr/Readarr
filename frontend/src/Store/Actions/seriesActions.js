826555c71cf0fa55ebcc07b09cd76133b3b47302
Robin Dadswell <robin@dadswell.email>
2021-01-03 23:03:52 +0000

    fixed seriesActions

--- frontend/src/Store/Actions/seriesActions.js
+++ frontend/src/Store/Actions/seriesActions.js
@@ -5,6 +5,7 @@ import createFetchHandler from './Creators/createFetchHandler';
 import createHandleActions from './Creators/createHandleActions';
 import createSetClientSideCollectionSortReducer from './Creators/Reducers/createSetClientSideCollectionSortReducer';
 import createSetSettingValueReducer from './Creators/Reducers/createSetSettingValueReducer';
+import createSetTableOptionReducer from './Creators/Reducers/createSetTableOptionReducer';
 
 //
 // Variables
@@ -104,7 +105,9 @@ export const SET_SERIES_VALUE = 'albums/setAlbumValue';
 
 export const fetchSeries = createThunk(FETCH_SERIES);
 export const setSeriesSort = createAction(SET_SERIES_SORT);
+export const setSeriesTableOption = createAction(SET_SERIES_TABLE_OPTION);
 export const clearSeries = createAction(CLEAR_SERIES);
+
 
 //
 // Action Handlers
@@ -119,6 +122,8 @@ export const actionHandlers = handleThunks({
 export const reducers = createHandleActions({
 
   [SET_SERIES_SORT]: createSetClientSideCollectionSortReducer(section),
+
+  [SET_SERIES_TABLE_OPTION]: createSetTableOptionReducer(section),
 
   [SET_SERIES_VALUE]: createSetSettingValueReducer(section),
 
