fa653bf54656ca407868f240a9d19508bb58dd10
Robin Dadswell <robin@dadswell.email>
2021-01-03 22:54:59 +0000

    health check selector fix

--- frontend/src/System/Status/Health/HealthStatusConnector.js
+++ frontend/src/System/Status/Health/HealthStatusConnector.js
@@ -8,6 +8,7 @@ import createHealthCheckSelector from 'Store/Selectors/createHealthCheckSelector
 
 function createMapStateToProps() {
   return createSelector(
+    createHealthCheckSelector(),
     (state) => state.app,
     (state) => state.system.health,
     (app, health) => {
