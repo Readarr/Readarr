54c1c7862e3dd11b314aef1e3dd1fe8a7e04fba9
ta264 <ta264@users.noreply.github.com>
2021-01-06 21:03:45 +0000

    New: Sort interactive import by path by default
    (cherry picked from commit 4e41c3b237de596496523fcd3671d8d3c4192c27)

--- frontend/src/Store/Actions/interactiveImportActions.js
+++ frontend/src/Store/Actions/interactiveImportActions.js
@@ -32,8 +32,8 @@ export const defaultState = {
   error: null,
   items: [],
   pendingChanges: {},
-  sortKey: 'quality',
-  sortDirection: sortDirections.DESCENDING,
+  sortKey: 'path',
+  sortDirection: sortDirections.ASCENDING,
   recentFolders: [],
   importMode: 'chooseImportMode',
   sortPredicates: {
