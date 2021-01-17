Sonarr/Sonarr

New: Added option to filter Release Profile to a specific indexer

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -60,7 +60,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
 
                     // The series will be the same as the one in history since it's the same episode.
                     // Instead of fetching the series from the DB reuse the known series.
-                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Series, mostRecent.SourceTitle);
+                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Series, mostRecent.SourceTitle, subject.Release?.IndexerId ?? 0);
 
                     var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
                         subject.Series.QualityProfile,

src/NzbDrone.Core/DecisionEngine/Specifications/UpgradeDiskSpecification.cs
@@ -38,7 +38,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
                                                            subject.Series.LanguageProfile, 
                                                            file.Quality, 
                                                            file.Language,
-                                                           _preferredWordServiceCalculator.Calculate(subject.Series, file.GetSceneOrFileName()),
+                                                           _preferredWordServiceCalculator.Calculate(subject.Series, file.GetSceneOrFileName(), subject.Release?.IndexerId ?? 0),
                                                            subject.ParsedEpisodeInfo.Quality, 
                                                            subject.ParsedEpisodeInfo.Language,
                                                            subject.PreferredWordScore))

src/NzbDrone.Core/Download/Aggregation/Aggregators/AggregatePreferredWordScore.cs
@@ -14,7 +14,7 @@ namespace NzbDrone.Core.Download.Aggregation.Aggregators
 
         public RemoteEpisode Aggregate(RemoteEpisode remoteEpisode)
         {
-            remoteEpisode.PreferredWordScore = _preferredWordServiceCalculator.Calculate(remoteEpisode.Series, remoteEpisode.Release.Title);
+            remoteEpisode.PreferredWordScore = _preferredWordServiceCalculator.Calculate(remoteEpisode.Series, remoteEpisode.Release.Title, remoteEpisode.Release.IndexerId);
 
             return remoteEpisode;
         }

src/NzbDrone.Core/Profiles/Releases/PreferredWordService.cs
@@ -8,7 +8,7 @@ namespace NzbDrone.Core.Profiles.Releases
 {
     public interface IPreferredWordService
     {
-        int Calculate(Series series, string title);
+        int Calculate(Series series, string title, int indexerId);
         List<string> GetMatchingPreferredWords(Series series, string title);
     }
 
@@ -25,11 +25,11 @@ namespace NzbDrone.Core.Profiles.Releases
             _logger = logger;
         }
 
-        public int Calculate(Series series, string title)
+        public int Calculate(Series series, string title, int indexerId)
         {
             _logger.Trace("Calculating preferred word score for '{0}'", title);
 
-            var releaseProfiles = _releaseProfileService.AllForTags(series.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(series.Tags, indexerId);
             var matchingPairs = new List<KeyValuePair<string, int>>();
 
             foreach (var releaseProfile in releaseProfiles)
@@ -54,7 +54,7 @@ namespace NzbDrone.Core.Profiles.Releases
 
         public List<string> GetMatchingPreferredWords(Series series, string title)
         {
-            var releaseProfiles = _releaseProfileService.AllForTags(series.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(series.Tags, 0);
             var matchingPairs = new List<KeyValuePair<string, int>>();
 
             _logger.Trace("Calculating preferred word score for '{0}'", title);

Readarr/Readarr

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -62,7 +62,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
 
                     // The author will be the same as the one in history since it's the same book.
                     // Instead of fetching the author from the DB reuse the known author.
-                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Author, mostRecent.SourceTitle);
+                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Author, mostRecent.SourceTitle, subject.Release?.IndexerId ?? 0);
 
                     var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
                         subject.Author.QualityProfile,

src/NzbDrone.Core/DecisionEngine/Specifications/UpgradeDiskSpecification.cs
@@ -40,7 +40,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                 if (!_upgradableSpecification.IsUpgradable(subject.Author.QualityProfile,
                                                            new List<QualityModel> { file.Quality },
-                                                           _preferredWordServiceCalculator.Calculate(subject.Author, file.GetSceneOrFileName()),
+                                                           _preferredWordServiceCalculator.Calculate(subject.Author, file.GetSceneOrFileName(), subject.Release?.IndexerId ?? 0),
                                                            subject.ParsedBookInfo.Quality,
                                                            subject.PreferredWordScore))
                 {

src/NzbDrone.Core/Download/Aggregation/Aggregators/AggregatePreferredWordScore.cs
@@ -14,7 +14,7 @@ namespace NzbDrone.Core.Download.Aggregation.Aggregators
 
         public RemoteBook Aggregate(RemoteBook remoteBook)
         {
-            remoteBook.PreferredWordScore = _preferredWordServiceCalculator.Calculate(remoteBook.Author, remoteBook.Release.Title);
+            remoteBook.PreferredWordScore = _preferredWordServiceCalculator.Calculate(remoteBook.Author, remoteBook.Release.Title, remoteBook.Release.IndexerId);
 
             return remoteBook;
         }

src/NzbDrone.Core/Profiles/Releases/PreferredWordService.cs
@@ -8,7 +8,7 @@ namespace NzbDrone.Core.Profiles.Releases
 {
     public interface IPreferredWordService
     {
-        int Calculate(Author author, string title);
+        int Calculate(Author author, string title, int indexerId);
         List<string> GetMatchingPreferredWords(Author author, string title);
     }
 
@@ -25,11 +25,11 @@ namespace NzbDrone.Core.Profiles.Releases
             _logger = logger;
         }
 
-        public int Calculate(Author series, string title)
+        public int Calculate(Author series, string title, int indexerId)
         {
             _logger.Trace("Calculating preferred word score for '{0}'", title);
 
-            var releaseProfiles = _releaseProfileService.AllForTags(series.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(series.Tags, indexerId);
             var matchingPairs = new List<KeyValuePair<string, int>>();
 
             foreach (var releaseProfile in releaseProfiles)
@@ -54,7 +54,7 @@ namespace NzbDrone.Core.Profiles.Releases
 
         public List<string> GetMatchingPreferredWords(Author author, string title)
         {
-            var releaseProfiles = _releaseProfileService.AllForTags(author.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(author.Tags, 0);
             var matchingPairs = new List<KeyValuePair<string, int>>();
 
             _logger.Trace("Calculating preferred word score for '{0}'", title);