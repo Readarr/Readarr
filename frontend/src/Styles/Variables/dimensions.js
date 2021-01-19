Lidarr/Lidarr

Fixed: Ended overlay on artist posters
Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

frontend/src/Styles/Variables/dimensions.js
@@ -47,7 +47,7 @@ module.exports = {
   modalBodyPadding: '30px',
 
   // Artist
-  artistIndexColumnPadding: '20px',
-  artistIndexColumnPaddingSmallScreen: '10px',
+  artistIndexColumnPadding: '10px',
+  artistIndexColumnPaddingSmallScreen: '5px',
   artistIndexOverviewInfoRowHeight: '21px'
 };

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

frontend/src/Styles/Variables/dimensions.js
@@ -46,8 +46,8 @@ module.exports = {
   // Modal
   modalBodyPadding: '30px',
 
-  // Author
-  authorIndexColumnPadding: '20px',
-  authorIndexColumnPaddingSmallScreen: '10px',
+  // Artist
+  authorIndexColumnPadding: '10px',
+  authorIndexColumnPaddingSmallScreen: '5px',
   authorIndexOverviewInfoRowHeight: '21px'
 };