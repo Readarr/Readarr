Lidarr/Lidarr

New: Add DownloadClient and DownloadId to Webhook notifications
Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

src/NzbDrone.Core/Notifications/Webhook/Webhook.cs
@@ -34,7 +34,9 @@ namespace NzbDrone.Core.Notifications.Webhook
                     QualityVersion = quality.Revision.Version,
                     ReleaseGroup = remoteAlbum.ParsedAlbumInfo.ReleaseGroup
                 }),
-                Release = new WebhookRelease(quality, remoteAlbum)
+                Release = new WebhookRelease(quality, remoteAlbum),
+                DownloadClient = message.DownloadClient,
+                DownloadId = message.DownloadId
             };
 
             _proxy.SendWebhook(payload, Settings);
@@ -56,7 +58,9 @@ namespace NzbDrone.Core.Notifications.Webhook
                     ReleaseGroup = x.ReleaseGroup
                 })).ToList(),
                 TrackFiles = trackFiles.ConvertAll(x => new WebhookTrackFile(x)),
-                IsUpgrade = message.OldFiles.Any()
+                IsUpgrade = message.OldFiles.Any(),
+                DownloadClient = message.DownloadClient,
+                DownloadId = message.DownloadId
             };
 
             _proxy.SendWebhook(payload, Settings);

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core/Notifications/Webhook/Webhook.cs
@@ -34,7 +34,9 @@ namespace NzbDrone.Core.Notifications.Webhook
                     QualityVersion = quality.Revision.Version,
                     ReleaseGroup = remoteBook.ParsedBookInfo.ReleaseGroup
                 }),
-                Release = new WebhookRelease(quality, remoteBook)
+                Release = new WebhookRelease(quality, remoteBook),
+                DownloadClient = message.DownloadClient,
+                DownloadId = message.DownloadId
             };
 
             _proxy.SendWebhook(payload, Settings);
@@ -47,10 +49,12 @@ namespace NzbDrone.Core.Notifications.Webhook
             var payload = new WebhookImportPayload
             {
                 EventType = "Download",
-                Author = new WebhookAuthor(message.Author),
+                Artist = new WebhookAuthor(message.Author),
                 Book = new WebhookBook(message.Book),
                 BookFiles = bookFiles.ConvertAll(x => new WebhookBookFile(x)),
-                IsUpgrade = message.OldFiles.Any()
+                IsUpgrade = message.OldFiles.Any(),
+                DownloadClient = message.DownloadClient,
+                DownloadId = message.DownloadId
             };
 
             _proxy.SendWebhook(payload, Settings);