5772573bd9be0ea28ee03453ac10284b8c3219a3
Robin Dadswell <robin@dadswell.email>
2021-01-19 12:57:20 +0000

    revert to author/book instaed of artists/album

--- src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
+++ src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -60,8 +60,8 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
                         continue;
                     }
 
-                    // The artist will be the same as the one in history since it's the same album.
-                    // Instead of fetching the artist from the DB reuse the known artist.
+                    // The author will be the same as the one in history since it's the same book.	                    // The artist will be the same as the one in history since it's the same album.
+                    // Instead of fetching the author from the DB reuse the known author.
                     var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Author, mostRecent.SourceTitle, subject.Release?.IndexerId ?? 0);
 
                     var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
