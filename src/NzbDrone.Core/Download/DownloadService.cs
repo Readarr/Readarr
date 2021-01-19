Sonarr/Sonarr

Fixed: Indexer being disabled due to download client rejecting it

src/NzbDrone.Core/Download/DownloadService.cs
@@ -81,6 +81,11 @@ namespace NzbDrone.Core.Download
                 _logger.Trace("Release {0} no longer available on indexer.", remoteEpisode);
                 throw;
             }
+            catch (DownloadClientRejectedReleaseException)
+            {
+                _logger.Trace("Release {0} rejected by download client, possible duplicate.", remoteEpisode);
+                throw;
+            }
             catch (ReleaseDownloadException ex)
             {
                 var http429 = ex.InnerException as TooManyRequestsException;

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core/Download/DownloadService.cs
@@ -80,6 +80,11 @@ namespace NzbDrone.Core.Download
                 _logger.Trace("Release {0} no longer available on indexer.", remoteBook);
                 throw;
             }
+            catch (DownloadClientRejectedReleaseException)
+            {
+                _logger.Trace("Release {0} rejected by download client, possible duplicate.", remoteAlbum);
+                throw;
+            }
             catch (ReleaseDownloadException ex)
             {
                 var http429 = ex.InnerException as TooManyRequestsException;