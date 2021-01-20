Lidarr/Lidarr

Fixed: Edge case where import fails due to DB relationship mismatch

src/NzbDrone.Core/MediaFiles/TrackImport/Specifications/SameFileSpecification.cs
@@ -15,9 +15,9 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Specifications
             _logger = logger;
         }
 
-        public Decision IsSatisfiedBy(LocalTrack item, DownloadClientItem downloadClientItem)
+        public Decision IsSatisfiedBy(LocalTrack localTrack, DownloadClientItem downloadClientItem)
         {
-            var trackFiles = item.Tracks.Where(e => e.TrackFileId != 0).Select(e => e.TrackFile).ToList();
+            var trackFiles = localTrack.Tracks.Where(e => e.TrackFileId != 0).Select(e => e.TrackFile).ToList();
 
             if (trackFiles.Count == 0)
             {
@@ -31,9 +31,19 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Specifications
                 return Decision.Accept();
             }
 
-            if (trackFiles.First().Value.Size == item.Size)
+            var trackFile = trackFiles.First().Value;
+
+            if (trackFile == null)
             {
-                _logger.Debug("'{0}' Has the same filesize as existing file", item.Path);
+                var track = localTrack.Tracks.First();
+                _logger.Trace("Unable to get track file details from the DB. TrackId: {0} TrackFileId: {1}", track.Id, track.TrackFileId);
+
+                return Decision.Accept();
+            }
+
+            if (trackFiles.First().Value.Size == localTrack.Size)
+            {
+                _logger.Debug("'{0}' Has the same filesize as existing file", localTrack.Path);
                 return Decision.Reject("Has the same filesize as existing file");
             }
 

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core/MediaFiles/BookImport/Specifications/SameFileSpecification.cs
@@ -15,9 +15,9 @@ namespace NzbDrone.Core.MediaFiles.BookImport.Specifications
             _logger = logger;
         }
 
-        public Decision IsSatisfiedBy(LocalBook item, DownloadClientItem downloadClientItem)
+        public Decision IsSatisfiedBy(LocalBook localBook, DownloadClientItem downloadClientItem)
         {
-            var bookFiles = item.Book?.BookFiles?.Value;
+            var bookFiles = localBook.Book?.BookFiles?.Value;
 
             if (bookFiles == null || !bookFiles.Any())
             {
@@ -27,9 +27,17 @@ namespace NzbDrone.Core.MediaFiles.BookImport.Specifications
 
             foreach (var bookFile in bookFiles)
             {
-                if (bookFile.Size == item.Size)
+                if (bookFile == null)
                 {
-                    _logger.Debug("'{0}' Has the same filesize as existing file", item.Path);
+                    var book = localBook.Book;
+                    _logger.Trace("Unable to get book file details from the DB. BookId: {0} BookFileId: {1}", book.Id, bookFile.Id);
+
+                    return Decision.Accept();
+                }
+
+                if (bookFile.Size == localBook.Size)
+                {
+                    _logger.Debug("'{0}' Has the same filesize as existing file", localBook.Path);
                     return Decision.Reject("Has the same filesize as existing file");
                 }
             }