Lidarr/Lidarr

Simplify ManualImportModule null check

src/Lidarr.Api.V1/ManualImport/ManualImportModule.cs
@@ -47,14 +47,11 @@ namespace Lidarr.Api.V1.ManualImport
             var downloadId = (string)Request.Query.downloadId;
             NzbDrone.Core.Music.Artist artist = null;
 
-            var artistIdQuery = Request.Query.artistId;
-            if (artistIdQuery.HasValue)
+            var artistIdQuery = Request.GetNullableIntegerQueryParameter("artistId", null);
+
+            if (artistIdQuery.HasValue && artistIdQuery.Value > 0)
             {
-                var artistId = Convert.ToInt32(artistIdQuery.Value);
-                if (artistId > 0)
-                {
-                    artist = _artistService.GetArtist(Convert.ToInt32(artistIdQuery.Value));
-                }
+                artist = _artistService.GetArtist(Convert.ToInt32(artistIdQuery.Value));
             }
 
             var filter = Request.GetBooleanQueryParameter("filterExistingFiles", true) ? FilterFilesType.Matched : FilterFilesType.None;

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/Readarr.Api.V1/ManualImport/ManualImportModule.cs
@@ -47,15 +47,11 @@ namespace Readarr.Api.V1.ManualImport
             var downloadId = (string)Request.Query.downloadId;
             NzbDrone.Core.Books.Author author = null;
 
-            var authorIdQuery = Request.Query.authorId;
-            if (authorIdQuery.HasValue)
-            {
-                var authorId = Convert.ToInt32(authorIdQuery.Value);
+            var authorIdQuery = Request.GetNullableIntegerQueryParameter("authorId", null);
 
-                if (authorId > 0)
-                {
-                    author = _authorService.GetAuthor(authorId);
-                }
+            if (authorIdQuery.HasValue && authorIdQuery.Value > 0)
+            {
+                author = _authorService.GetAuthor(Convert.ToInt32(authorIdQuery.Value));
             }
 
             var filter = Request.GetBooleanQueryParameter("filterExistingFiles", true) ? FilterFilesType.Matched : FilterFilesType.None;