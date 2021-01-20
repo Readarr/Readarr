Lidarr/Lidarr

Fixed: Sorting of queue by artist title when unknown items are included
Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

src/Lidarr.Api.V1/Queue/QueueModule.cs
@@ -119,15 +119,15 @@ namespace Lidarr.Api.V1.Queue
                 case "status":
                     return q => q.Status;
                 case "artists.sortName":
-                    return q => q.Artist?.SortName;
+                    return q => q.Artist?.SortName ?? string.Empty;
                 case "title":
                     return q => q.Title;
                 case "album":
                     return q => q.Album;
                 case "albums.title":
-                    return q => q.Album?.Title;
+                    return q => q.Album?.Title ?? string.Empty;
                 case "album.releaseDate":
-                    return q => q.Album?.ReleaseDate;
+                    return q => q.Album?.ReleaseDate ?? DateTime.MinValue;
                 case "quality":
                     return q => q.Quality;
                 case "progress":

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/Readarr.Api.V1/Queue/QueueModule.cs
@@ -119,15 +119,15 @@ namespace Readarr.Api.V1.Queue
                 case "status":
                     return q => q.Status;
                 case "authors.sortName":
-                    return q => q.Author?.SortName;
+                    return q => q.Author?.SortName ?? string.Empty;
                 case "title":
                     return q => q.Title;
                 case "book":
                     return q => q.Book;
                 case "book.title":
-                    return q => q.Book?.Title;
+                    return q => q.Book?.Title ?? string.Empty;
                 case "book.releaseDate":
-                    return q => q.Book?.ReleaseDate;
+                    return q => q.Book?.ReleaseDate ?? DateTime.MinValue;
                 case "quality":
                     return q => q.Quality;
                 case "progress":