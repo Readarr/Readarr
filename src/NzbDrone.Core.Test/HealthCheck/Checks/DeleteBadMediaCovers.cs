Lidarr/Lidarr

Improve use of All() for Path related queries

src/NzbDrone.Core.Test/HealthCheck/Checks/DeleteBadMediaCovers.cs
@@ -34,8 +34,8 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
                .Build().ToList();
 
             Mocker.GetMock<IArtistService>()
-                .Setup(c => c.GetAllArtists())
-                .Returns(_artist);
+                .Setup(c => c.AllArtistPaths())
+                .Returns(_artist.ToDictionary(x => x.Id, x => x.Path));
 
             Mocker.GetMock<IMetadataFileService>()
                 .Setup(c => c.GetFilesByArtist(_artist.First().Id))
@@ -73,7 +73,7 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
             Subject.Clean();
 
             Mocker.GetMock<IConfigService>().VerifySet(c => c.CleanupMetadataImages = true, Times.Never());
-            Mocker.GetMock<IArtistService>().Verify(c => c.GetAllArtists(), Times.Never());
+            Mocker.GetMock<IArtistService>().Verify(c => c.AllArtistPaths(), Times.Never());
 
             AssertImageWasNotRemoved();
         }

src/NzbDrone.Core/Housekeeping/Housekeepers/DeleteBadMediaCovers.cs
@@ -38,19 +38,19 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
                 return;
             }
 
-            var artists = _artistService.GetAllArtists();
+            var artists = _artistService.AllArtistPaths();
             var imageExtensions = new List<string> { ".jpg", ".png", ".gif" };
 
             foreach (var artist in artists)
             {
-                var images = _metaFileService.GetFilesByArtist(artist.Id)
+                var images = _metaFileService.GetFilesByArtist(artist.Key)
                     .Where(c => c.LastUpdated > new DateTime(2014, 12, 27) && imageExtensions.Any(x => c.RelativePath.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)));
 
                 foreach (var image in images)
                 {
                     try
                     {
-                        var path = Path.Combine(artist.Path, image.RelativePath);
+                        var path = Path.Combine(artist.Value, image.RelativePath);
                         if (!IsValid(path))
                         {
                             _logger.Debug("Deleting invalid image file " + path);

src/NzbDrone.Core/MediaFiles/MediaFileDeletionService.cs
@@ -104,22 +104,22 @@ namespace NzbDrone.Core.MediaFiles
             if (message.DeleteFiles)
             {
                 var artist = message.Artist;
-                var allArtists = _artistService.GetAllArtists();
+                var allArtists = _artistService.AllArtistPaths();
 
                 foreach (var s in allArtists)
                 {
-                    if (s.Id == artist.Id)
+                    if (s.Key == artist.Id)
                     {
                         continue;
                     }
 
-                    if (artist.Path.IsParentPath(s.Path))
+                    if (artist.Path.IsParentPath(s.Value))
                     {
                         _logger.Error("Artist path: '{0}' is a parent of another artist, not deleting files.", artist.Path);
                         return;
                     }
 
-                    if (artist.Path.PathEquals(s.Path))
+                    if (artist.Path.PathEquals(s.Value))
                     {
                         _logger.Error("Artist path: '{0}' is the same as another artist, not deleting files.", artist.Path);
                         return;

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core.Test/HealthCheck/Checks/DeleteBadMediaCovers.cs
@@ -33,9 +33,9 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
             _metadata = Builder<MetadataFile>.CreateListOfSize(1)
                .Build().ToList();
 
-            Mocker.GetMock<IAuthorService>()
-                .Setup(c => c.GetAllAuthors())
-                .Returns(_artist);
+            Mocker.GetMock<IArtistService>()
+                .Setup(c => c.AllArtistPaths())
+                .Returns(_artist.ToDictionary(x => x.Id, x => x.Path));
 
             Mocker.GetMock<IMetadataFileService>()
                 .Setup(c => c.GetFilesByAuthor(_artist.First().Id))
@@ -73,7 +73,7 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
             Subject.Clean();
 
             Mocker.GetMock<IConfigService>().VerifySet(c => c.CleanupMetadataImages = true, Times.Never());
-            Mocker.GetMock<IAuthorService>().Verify(c => c.GetAllAuthors(), Times.Never());
+            Mocker.GetMock<IArtistService>().Verify(c => c.GetAllAuthors(), Times.Never());
 
             AssertImageWasNotRemoved();
         }

src/NzbDrone.Core/Housekeeping/Housekeepers/DeleteBadMediaCovers.cs
@@ -43,14 +43,14 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
 
             foreach (var author in artists)
             {
-                var images = _metaFileService.GetFilesByAuthor(author.Id)
+                var images = _metaFileService.GetFilesByAuthor(author.Key)
                     .Where(c => c.LastUpdated > new DateTime(2014, 12, 27) && imageExtensions.Any(x => c.RelativePath.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)));
 
                 foreach (var image in images)
                 {
                     try
                     {
-                        var path = Path.Combine(author.Path, image.RelativePath);
+                        var path = Path.Combine(author.Value, image.RelativePath);
                         if (!IsValid(path))
                         {
                             _logger.Debug("Deleting invalid image file " + path);

src/NzbDrone.Core/MediaFiles/MediaFileDeletionService.cs
@@ -104,22 +104,22 @@ namespace NzbDrone.Core.MediaFiles
             if (message.DeleteFiles)
             {
                 var author = message.Author;
-                var allArtists = _authorService.GetAllAuthors();
+                var allArtists = _artistService.GetAllAuthors();
 
                 foreach (var s in allArtists)
                 {
-                    if (s.Id == author.Id)
+                    if (s.Key == artist.Id)
                     {
                         continue;
                     }
 
-                    if (author.Path.IsParentPath(s.Path))
+                    if (author.Path.IsParentPath(s.Value))
                     {
                         _logger.Error("Author path: '{0}' is a parent of another author, not deleting files.", author.Path);
                         return;
                     }
 
-                    if (author.Path.PathEquals(s.Path))
+                    if (author.Path.PathEquals(s.Value))
                     {
                         _logger.Error("Author path: '{0}' is the same as another author, not deleting files.", author.Path);
                         return;