Lidarr/Lidarr

Update MonitorToggleButton.js
Fix toggle monitored state tooltip spelling.

frontend/src/Components/MonitorToggleButton.js
@@ -7,7 +7,7 @@ import styles from './MonitorToggleButton.css';
 
 function getTooltip(monitored, isDisabled) {
   if (isDisabled) {
-    return 'Cannot toogle monitored state when artist is unmonitored';
+    return 'Cannot toggle monitored state when artist is unmonitored';
   }
 
   if (monitored) {

Readarr/Readarr

frontend/src/Components/MonitorToggleButton.js
@@ -7,7 +7,7 @@ import styles from './MonitorToggleButton.css';
 
 function getTooltip(monitored, isDisabled) {
   if (isDisabled) {
-    return 'Cannot toogle monitored state when author is unmonitored';
+    return 'Cannot toggle monitored state when author is unmonitored';
   }
 
   if (monitored) {