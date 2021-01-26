Sonarr/Sonarr

New: Clone indexer button
Closes #3546

frontend/src/Settings/Indexers/Indexers/IndexersConnector.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import sortByName from 'Utilities/Array/sortByName';
 import createSortedSectionSelector from 'Store/Selectors/createSortedSectionSelector';
-import { fetchIndexers, deleteIndexer } from 'Store/Actions/settingsActions';
+import { fetchIndexers, deleteIndexer, cloneIndexer } from 'Store/Actions/settingsActions';
 import Indexers from './Indexers';
 
 function createMapStateToProps() {
@@ -15,8 +15,9 @@ function createMapStateToProps() {
 }
 
 const mapDispatchToProps = {
-  fetchIndexers,
-  deleteIndexer
+  dispatchFetchIndexers: fetchIndexers,
+  dispatchDeleteIndexer: deleteIndexer,
+  dispatchCloneIndexer: cloneIndexer
 };
 
 class IndexersConnector extends Component {
@@ -25,14 +26,14 @@ class IndexersConnector extends Component {
   // Lifecycle
 
   componentDidMount() {
-    this.props.fetchIndexers();
+    this.props.dispatchFetchIndexers();
   }
 
   //
   // Listeners
 
   onConfirmDeleteIndexer = (id) => {
-    this.props.deleteIndexer({ id });
+    this.props.dispatchDeleteIndexer({ id });
   }
 
   //
@@ -49,8 +50,9 @@ class IndexersConnector extends Component {
 }
 
 IndexersConnector.propTypes = {
-  fetchIndexers: PropTypes.func.isRequired,
-  deleteIndexer: PropTypes.func.isRequired
+  dispatchFetchIndexers: PropTypes.func.isRequired,
+  dispatchDeleteIndexer: PropTypes.func.isRequired,
+  dispatchCloneIndexer: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(IndexersConnector);

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

frontend/src/Settings/Indexers/Indexers/IndexersConnector.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { deleteIndexer, fetchIndexers } from 'Store/Actions/settingsActions';
+import { cloneIndexer, deleteIndexer, fetchIndexers } from 'Store/Actions/settingsActions';
 import createSortedSectionSelector from 'Store/Selectors/createSortedSectionSelector';
 import sortByName from 'Utilities/Array/sortByName';
 import Indexers from './Indexers';
@@ -15,8 +15,9 @@ function createMapStateToProps() {
 }
 
 const mapDispatchToProps = {
-  fetchIndexers,
-  deleteIndexer
+  dispatchFetchIndexers: fetchIndexers,
+  dispatchDeleteIndexer: deleteIndexer,
+  dispatchCloneIndexer: cloneIndexer
 };
 
 class IndexersConnector extends Component {
@@ -25,14 +26,14 @@ class IndexersConnector extends Component {
   // Lifecycle
 
   componentDidMount() {
-    this.props.fetchIndexers();
+    this.props.dispatchFetchIndexers();
   }
 
   //
   // Listeners
 
   onConfirmDeleteIndexer = (id) => {
-    this.props.deleteIndexer({ id });
+    this.props.dispatchDeleteIndexer({ id });
   }
 
   //
@@ -49,8 +50,9 @@ class IndexersConnector extends Component {
 }
 
 IndexersConnector.propTypes = {
-  fetchIndexers: PropTypes.func.isRequired,
-  deleteIndexer: PropTypes.func.isRequired
+  dispatchFetchIndexers: PropTypes.func.isRequired,
+  dispatchDeleteIndexer: PropTypes.func.isRequired,
+  dispatchCloneIndexer: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(IndexersConnector);