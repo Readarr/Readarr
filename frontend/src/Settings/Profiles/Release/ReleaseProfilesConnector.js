Sonarr/Sonarr

New: Added option to filter Release Profile to a specific indexer

frontend/src/Settings/Profiles/Release/ReleaseProfilesConnector.js
@@ -2,24 +2,28 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { fetchReleaseProfiles, deleteReleaseProfile } from 'Store/Actions/settingsActions';
+import { fetchReleaseProfiles, deleteReleaseProfile, fetchIndexers } from 'Store/Actions/settingsActions';
 import createTagsSelector from 'Store/Selectors/createTagsSelector';
 import ReleaseProfiles from './ReleaseProfiles';
 
 function createMapStateToProps() {
   return createSelector(
     (state) => state.settings.releaseProfiles,
+    (state) => state.settings.indexers,
     createTagsSelector(),
-    (releaseProfiles, tagList) => {
+    (releaseProfiles, indexers, tagList) => {
       return {
         ...releaseProfiles,
-        tagList
+        tagList,
+        isIndexersPopulated: indexers.isPopulated,
+        indexerList: indexers.items
       };
     }
   );
 }
 
 const mapDispatchToProps = {
+  fetchIndexers,
   fetchReleaseProfiles,
   deleteReleaseProfile
 };
@@ -31,6 +35,9 @@ class ReleaseProfilesConnector extends Component {
 
   componentDidMount() {
     this.props.fetchReleaseProfiles();
+    if (!this.props.isIndexersPopulated) {
+      this.props.fetchIndexers();
+    }
   }
 
   //
@@ -54,8 +61,10 @@ class ReleaseProfilesConnector extends Component {
 }
 
 ReleaseProfilesConnector.propTypes = {
+  isIndexersPopulated: PropTypes.bool.isRequired,
   fetchReleaseProfiles: PropTypes.func.isRequired,
-  deleteReleaseProfile: PropTypes.func.isRequired
+  deleteReleaseProfile: PropTypes.func.isRequired,
+  fetchIndexers: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(ReleaseProfilesConnector);

src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
@@ -41,7 +41,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
                                                            languageProfile, 
                                                            file.Quality, 
                                                            file.Language,
-                                                           _preferredWordServiceCalculator.Calculate(subject.Series, file.GetSceneOrFileName()),
+                                                           _preferredWordServiceCalculator.Calculate(subject.Series, file.GetSceneOrFileName(), subject.Release.IndexerId),
                                                            subject.ParsedEpisodeInfo.Quality,
                                                            subject.PreferredWordScore))
                 {

src/NzbDrone.Core/DecisionEngine/Specifications/ReleaseRestrictionsSpecification.cs
@@ -30,10 +30,10 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
             _logger.Debug("Checking if release meets restrictions: {0}", subject);
 
             var title = subject.Release.Title;
-            var restrictions = _releaseProfileService.AllForTags(subject.Series.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(subject.Series.Tags, subject.Release.IndexerId);
 
-            var required = restrictions.Where(r => r.Required.IsNotNullOrWhiteSpace());
-            var ignored = restrictions.Where(r => r.Ignored.IsNotNullOrWhiteSpace());
+            var required = releaseProfiles.Where(r => r.Required.IsNotNullOrWhiteSpace());
+            var ignored = releaseProfiles.Where(r => r.Ignored.IsNotNullOrWhiteSpace());
 
             foreach (var r in required)
             {

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

frontend/src/Settings/Profiles/Release/ReleaseProfilesConnector.js
@@ -2,24 +2,28 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { deleteReleaseProfile, fetchReleaseProfiles } from 'Store/Actions/settingsActions';
+import { deleteReleaseProfile, fetchIndexers, fetchReleaseProfiles } from 'Store/Actions/settingsActions';
 import createTagsSelector from 'Store/Selectors/createTagsSelector';
 import ReleaseProfiles from './ReleaseProfiles';
 
 function createMapStateToProps() {
   return createSelector(
     (state) => state.settings.releaseProfiles,
+    (state) => state.settings.indexers,
     createTagsSelector(),
-    (releaseProfiles, tagList) => {
+    (releaseProfiles, indexers, tagList) => {
       return {
         ...releaseProfiles,
-        tagList
+        tagList,
+        isIndexersPopulated: indexers.isPopulated,
+        indexerList: indexers.items
       };
     }
   );
 }
 
 const mapDispatchToProps = {
+  fetchIndexers,
   fetchReleaseProfiles,
   deleteReleaseProfile
 };
@@ -31,6 +35,9 @@ class ReleaseProfilesConnector extends Component {
 
   componentDidMount() {
     this.props.fetchReleaseProfiles();
+    if (!this.props.isIndexersPopulated) {
+      this.props.fetchIndexers();
+    }
   }
 
   //
@@ -54,8 +61,10 @@ class ReleaseProfilesConnector extends Component {
 }
 
 ReleaseProfilesConnector.propTypes = {
+  isIndexersPopulated: PropTypes.bool.isRequired,
   fetchReleaseProfiles: PropTypes.func.isRequired,
-  deleteReleaseProfile: PropTypes.func.isRequired
+  deleteReleaseProfile: PropTypes.func.isRequired,
+  fetchIndexers: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(ReleaseProfilesConnector);

src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
@@ -40,7 +40,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                 if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
                                                            currentQualities,
-                                                           _preferredWordServiceCalculator.Calculate(subject.Author, file.GetSceneOrFileName()),
+                                                           _preferredWordServiceCalculator.Calculate(subject.Author, file.GetSceneOrFileName(), subject.Release.IndexerId),
                                                            subject.ParsedBookInfo.Quality,
                                                            subject.PreferredWordScore))
                 {

src/NzbDrone.Core/DecisionEngine/Specifications/ReleaseRestrictionsSpecification.cs
@@ -30,10 +30,10 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
             _logger.Debug("Checking if release meets restrictions: {0}", subject);
 
             var title = subject.Release.Title;
-            var restrictions = _releaseProfileService.AllForTags(subject.Author.Tags);
+            var releaseProfiles = _releaseProfileService.EnabledForTags(subject.Author.Tags, subject.Release.IndexerId);
 
-            var required = restrictions.Where(r => r.Required.IsNotNullOrWhiteSpace());
-            var ignored = restrictions.Where(r => r.Ignored.IsNotNullOrWhiteSpace());
+            var required = releaseProfiles.Where(r => r.Required.IsNotNullOrWhiteSpace());
+            var ignored = releaseProfiles.Where(r => r.Ignored.IsNotNullOrWhiteSpace());
 
             foreach (var r in required)
             {