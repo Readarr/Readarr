Sonarr/Sonarr

New: Health events for Webhooks

src/NzbDrone.Core/Notifications/Webhook/WebhookGrabPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookGrabPayload : WebhookPayload
     {
+        public WebhookSeries Series { get; set; }
         public List<WebhookEpisode> Episodes { get; set; }
         public WebhookRelease Release { get; set; }
         public string DownloadClient { get; set; }

src/NzbDrone.Core/Notifications/Webhook/WebhookImportPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookImportPayload : WebhookPayload
     {
+        public WebhookSeries Series { get; set; }
         public List<WebhookEpisode> Episodes { get; set; }
         public WebhookEpisodeFile EpisodeFile { get; set; }
         public bool IsUpgrade { get; set; }

Readarr/Readarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core/Notifications/Webhook/WebhookGrabPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookGrabPayload : WebhookPayload
     {
+        public WebhookAuthor Author { get; set; }
         public List<WebhookBook> Books { get; set; }
         public WebhookRelease Release { get; set; }
         public string DownloadClient { get; set; }

src/NzbDrone.Core/Notifications/Webhook/WebhookImportPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookImportPayload : WebhookPayload
     {
+        public WebhookAuthor Author { get; set; }
         public WebhookBook Book { get; set; }
         public List<WebhookBookFile> BookFiles { get; set; }
         public bool IsUpgrade { get; set; }