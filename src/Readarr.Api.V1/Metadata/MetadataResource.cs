c2fd37ca3c96eaf29174ef28b64657ebbc975817
ta264 <ta264@users.noreply.github.com>
2021-02-10 21:52:48 +0000

    Fixed: Adding indexers from presets

--- src/Readarr.Api.V1/Metadata/MetadataResource.cs
+++ src/Readarr.Api.V1/Metadata/MetadataResource.cs
@@ -2,7 +2,7 @@
 
 namespace Readarr.Api.V1.Metadata
 {
-    public class MetadataResource : ProviderResource
+    public class MetadataResource : ProviderResource<MetadataResource>
     {
         public bool Enable { get; set; }
     }
