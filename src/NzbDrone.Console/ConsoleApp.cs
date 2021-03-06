759b78e816d615fea4f5105c96728368e2d921ee
Taloth Saldono <Taloth@users.noreply.github.com>
2021-03-06 22:47:02 +0100

    Added mechanism for package maintainers to produce a health check error.
    Fixes #859
    
    (cherry picked from commit 7da02c236aa03e6aef011130526040c1cb8399fc)
    (cherry picked from commit 024000275df3b2d3b884c2c2fbf0b86bd36a631a)

--- src/NzbDrone.Console/ConsoleApp.cs
+++ src/NzbDrone.Console/ConsoleApp.cs
@@ -62,7 +62,7 @@ namespace NzbDrone.Console
                 {
                     System.Console.WriteLine("");
                     System.Console.WriteLine("");
-                    Logger.Fatal(ex.Message + " This can happen if another instance of Radarr is already running another application is using the same port (default: 7878) or the user has insufficient permissions");
+                    Logger.Fatal(ex.Message + " This can happen if another instance of Readarr is already running another application is using the same port (default: 8787) or the user has insufficient permissions");
                     Exit(ExitCodes.RecoverableFailure, startupArgs);
                 }
                 else
