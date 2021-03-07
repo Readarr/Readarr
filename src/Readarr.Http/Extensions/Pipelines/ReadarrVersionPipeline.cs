4c9f35d4859e7bd76ad84e1102ca02513acfc7b4
Mark McDowall <mark@mcdowall.ca>
2021-03-07 14:47:09 -0800

    Just one Application Version header
    (cherry picked from commit ab502ffda41aaae8bccaa4f33c62e3fca61b277f)

--- src/Readarr.Http/Extensions/Pipelines/ReadarrVersionPipeline.cs
+++ src/Readarr.Http/Extensions/Pipelines/ReadarrVersionPipeline.cs
@@ -15,9 +15,9 @@ namespace Readarr.Http.Extensions.Pipelines
 
         private void Handle(NancyContext context)
         {
-            if (!context.Response.Headers.ContainsKey("X-ApplicationVersion"))
+            if (!context.Response.Headers.ContainsKey("X-Application-Version"))
             {
-                context.Response.Headers.Add("X-ApplicationVersion", BuildInfo.Version.ToString());
+                context.Response.Headers.Add("X-Application-Version", BuildInfo.Version.ToString());
             }
         }
     }
