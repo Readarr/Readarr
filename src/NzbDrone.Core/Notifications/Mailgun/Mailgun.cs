Sonarr/Sonarr

New: Mailgun connection

src/NzbDrone.Core/Notifications/Mailgun/Mailgun.cs
@@ -1,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NLog;
+
+namespace NzbDrone.Core.Notifications.Mailgun
+{
+    public class MailGun : NotificationBase<MailgunSettings>
+    {
+        private readonly IMailgunProxy _proxy;
+        private readonly Logger _logger;
+        
+        public MailGun(IMailgunProxy proxy, Logger logger)
+        {
+            _proxy = proxy;
+            _logger = logger;
+        }
+
+        public override string Name => "Mailgun";
+        public override string Link => "https://mailgun.com";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(EPISODE_GRABBED_TITLE, grabMessage.Message, Settings);
+        }
+
+        public override void OnDownload(DownloadMessage downloadMessage)
+        {
+            _proxy.SendNotification(EPISODE_DOWNLOADED_TITLE, downloadMessage.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheckMessage)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheckMessage.Message, Settings);
+        }
+
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            try
+            {
+                const string title = "Test Notification";
+                const string body = "This is a test message from Sonarr, though Mailgun.";
+
+                _proxy.SendNotification(title, body, Settings);
+                _logger.Info("Successsfully sent email though Mailgun.");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test message though Mailgun.");
+                failures.Add(new ValidationFailure("", "Unable to send test message though Mailgun."));
+            }
+            
+            return new ValidationResult(failures);
+        }
+    }
+}

Readarr/Readarr

(cherry picked from commit 55752a6c6213c1d83d347ba0f6870aa6c1cc0770)
(cherry picked from commit 0b60ca68fcb5778d7b9eb1ae23e298458b4ab98d)
closes #981
closes #980

src/NzbDrone.Core/Notifications/Mailgun/Mailgun.cs
@@ -1,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NLog;
+
+namespace NzbDrone.Core.Notifications.Mailgun
+{
+    public class MailGun : NotificationBase<MailgunSettings>
+    {
+        private readonly IMailgunProxy _proxy;
+        private readonly Logger _logger;
+
+        public MailGun(IMailgunProxy proxy, Logger logger)
+        {
+            _proxy = proxy;
+            _logger = logger;
+        }
+
+        public override string Name => "Mailgun";
+        public override string Link => "https://mailgun.com";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(BOOK_GRABBED_TITLE, grabMessage.Message, Settings);
+        }
+
+        public override void OnReleaseImport(BookDownloadMessage downloadMessage)
+        {
+            _proxy.SendNotification(BOOK_DOWNLOADED_TITLE, downloadMessage.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheckMessage)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheckMessage.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            try
+            {
+                const string title = "Test Notification";
+                const string body = "This is a test message from Readarr, though Mailgun.";
+
+                _proxy.SendNotification(title, body, Settings);
+                _logger.Info("Successsfully sent email though Mailgun.");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test message though Mailgun.");
+                failures.Add(new ValidationFailure("", "Unable to send test message though Mailgun."));
+            }
+
+            return new ValidationResult(failures);
+        }
+    }
+}