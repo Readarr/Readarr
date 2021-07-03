Lidarr/Lidarr

Multiple Email Address
Fixes #2043
Fixes #2074
Fixes #2138
Fixes #2145
Fixes #2148

src/NzbDrone.Core.Test/NotificationTests/EmailTests/EmailSettingsValidatorFixture.cs
@@ -1,0 +1,103 @@
+using System;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Notifications.Email;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.NotificationTests.EmailTests
+{
+    [TestFixture]
+    public class EmailSettingsValidatorFixture : CoreTest<EmailSettingsValidator>
+    {
+        private EmailSettings _emailSettings;
+        private TestValidator<EmailSettings> _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new TestValidator<EmailSettings>
+                            {
+                                v => v.RuleFor(s => s).SetValidator(Subject)
+                            };
+
+            _emailSettings = Builder<EmailSettings>.CreateNew()
+                                        .With(s => s.Server = "someserver")
+                                        .With(s => s.Port = 567)
+                                        .With(s => s.From = "lidarr@lidarr.audio")
+                                        .With(s => s.To = new string[] { "lidarr@lidarr.audio" })
+                                        .Build();
+        }
+
+        [Test]
+        public void should_be_valid_if_all_settings_valid()
+        {
+            _validator.Validate(_emailSettings).IsValid.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_port_is_out_of_range()
+        {
+            _emailSettings.Port = 900000;
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_server_is_empty()
+        {
+            _emailSettings.Server = "";
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_from_is_empty()
+        {
+            _emailSettings.From = "";
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("lidarr")]
+        [TestCase("lidarr@lidarr")]
+        [TestCase("lidarr.audio")]
+        public void should_not_be_valid_if_to_is_invalid(string email)
+        {
+            _emailSettings.To = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("lidarr")]
+        [TestCase("lidarr@lidarr")]
+        [TestCase("lidarr.audio")]
+        public void should_not_be_valid_if_cc_is_invalid(string email)
+        {
+            _emailSettings.CC = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("lidarr")]
+        [TestCase("lidarr@lidarr")]
+        [TestCase("lidarr.audio")]
+        public void should_not_be_valid_if_bcc_is_invalid(string email)
+        {
+            _emailSettings.Bcc = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_to_bcc_cc_are_all_empty()
+        {
+            _emailSettings.To = Array.Empty<string>();
+            _emailSettings.CC = Array.Empty<string>();
+            _emailSettings.Bcc = Array.Empty<string>();
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+    }
+}

src/NzbDrone.Core/Notifications/Email/Email.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation.Results;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using NLog;
 using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Core.Notifications.Email
 {
     public class Email : NotificationBase<EmailSettings>
     {
-        private readonly IEmailService _emailService;
+        private readonly Logger _logger;
 
         public override string Name => "Email";
 
-        public Email(IEmailService emailService)
+        public Email(Logger logger)
         {
-            _emailService = emailService;
+            _logger = logger;
         }
 
         public override string Link => null;
@@ -21,38 +27,143 @@ namespace NzbDrone.Core.Notifications.Email
         {
             var body = $"{grabMessage.Message} sent to queue.";
 
-            _emailService.SendEmail(Settings, ALBUM_GRABBED_TITLE_BRANDED, body);
+            SendEmail(Settings, ALBUM_GRABBED_TITLE_BRANDED, body);
         }
 
         public override void OnReleaseImport(AlbumDownloadMessage message)
         {
             var body = $"{message.Message} Downloaded and sorted.";
 
-            _emailService.SendEmail(Settings, ALBUM_DOWNLOADED_TITLE_BRANDED, body);
+            SendEmail(Settings, ALBUM_DOWNLOADED_TITLE_BRANDED, body);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck message)
         {
-            _emailService.SendEmail(Settings, HEALTH_ISSUE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, HEALTH_ISSUE_TITLE_BRANDED, message.Message);
         }
 
         public override void OnDownloadFailure(DownloadFailedMessage message)
         {
-            _emailService.SendEmail(Settings, DOWNLOAD_FAILURE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, DOWNLOAD_FAILURE_TITLE_BRANDED, message.Message);
         }
 
         public override void OnImportFailure(AlbumDownloadMessage message)
         {
-            _emailService.SendEmail(Settings, IMPORT_FAILURE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, IMPORT_FAILURE_TITLE_BRANDED, message.Message);
         }
 
         public override ValidationResult Test()
         {
             var failures = new List<ValidationFailure>();
 
-            failures.AddIfNotNull(_emailService.Test(Settings));
+            failures.AddIfNotNull(Test(Settings));
 
             return new ValidationResult(failures);
+        }
+
+        public ValidationFailure Test(EmailSettings settings)
+        {
+            const string body = "Success! You have properly configured your email notification settings";
+
+            try
+            {
+                SendEmail(settings, "Radarr - Test Notification", body);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test email");
+                return new ValidationFailure("Server", "Unable to send test email");
+            }
+
+            return null;
+        }
+
+        private void SendEmail(EmailSettings settings, string subject, string body, bool htmlBody = false)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(ParseAddress("From", settings.From));
+            email.To.AddRange(settings.To.Select(x => ParseAddress("To", x)));
+            email.Cc.AddRange(settings.CC.Select(x => ParseAddress("CC", x)));
+            email.Bcc.AddRange(settings.Bcc.Select(x => ParseAddress("BCC", x)));
+
+            email.Subject = subject;
+            email.Body = new TextPart(htmlBody ? "html" : "plain")
+            {
+                Text = body
+            };
+
+            _logger.Debug("Sending email Subject: {0}", subject);
+
+            try
+            {
+                Send(email, settings);
+                _logger.Debug("Email sent. Subject: {0}", subject);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error sending email. Subject: {0}", email.Subject);
+                _logger.Debug(ex, ex.Message);
+                throw;
+            }
+
+            _logger.Debug("Finished sending email. Subject: {0}", email.Subject);
+        }
+
+        private void Send(MimeMessage email, EmailSettings settings)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Timeout = 10000;
+
+                var serverOption = SecureSocketOptions.Auto;
+
+                if (settings.RequireEncryption)
+                {
+                    if (settings.Port == 465)
+                    {
+                        serverOption = SecureSocketOptions.SslOnConnect;
+                    }
+                    else
+                    {
+                        serverOption = SecureSocketOptions.StartTls;
+                    }
+                }
+
+                _logger.Debug("Connecting to mail server");
+
+                client.Connect(settings.Server, settings.Port, serverOption);
+
+                if (!string.IsNullOrWhiteSpace(settings.Username))
+                {
+                    _logger.Debug("Authenticating to mail server");
+
+                    client.Authenticate(settings.Username, settings.Password);
+                }
+
+                _logger.Debug("Sending to mail server");
+
+                client.Send(email);
+
+                _logger.Debug("Sent to mail server, disconnecting");
+
+                client.Disconnect(true);
+
+                _logger.Debug("Disconnecting from mail server");
+            }
+        }
+
+        private MailboxAddress ParseAddress(string type, string address)
+        {
+            try
+            {
+                return MailboxAddress.Parse(address);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "{0} email address '{1}' invalid", type, address);
+                throw;
+            }
         }
     }
 }

Readarr/Readarr

Fixes #854
Fixes #884
Fixes #951
Fixes #954
(cherry picked from commit a8b6f70be1860aa502795f0dd30299c87d54dbbe)

src/NzbDrone.Core.Test/NotificationTests/EmailTests/EmailSettingsValidatorFixture.cs
@@ -1,0 +1,103 @@
+using System;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Notifications.Email;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.NotificationTests.EmailTests
+{
+    [TestFixture]
+    public class EmailSettingsValidatorFixture : CoreTest<EmailSettingsValidator>
+    {
+        private EmailSettings _emailSettings;
+        private TestValidator<EmailSettings> _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new TestValidator<EmailSettings>
+                            {
+                                v => v.RuleFor(s => s).SetValidator(Subject)
+                            };
+
+            _emailSettings = Builder<EmailSettings>.CreateNew()
+                                        .With(s => s.Server = "someserver")
+                                        .With(s => s.Port = 567)
+                                        .With(s => s.From = "readarr@readarr.com")
+                                        .With(s => s.To = new string[] { "readarr@readarr.com" })
+                                        .Build();
+        }
+
+        [Test]
+        public void should_be_valid_if_all_settings_valid()
+        {
+            _validator.Validate(_emailSettings).IsValid.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_port_is_out_of_range()
+        {
+            _emailSettings.Port = 900000;
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_server_is_empty()
+        {
+            _emailSettings.Server = "";
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_from_is_empty()
+        {
+            _emailSettings.From = "";
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("readarr")]
+        [TestCase("readarr@readarr")]
+        [TestCase("readarr.com")]
+        public void should_not_be_valid_if_to_is_invalid(string email)
+        {
+            _emailSettings.To = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("readarr")]
+        [TestCase("readarr@readarr")]
+        [TestCase("readarr.com")]
+        public void should_not_be_valid_if_cc_is_invalid(string email)
+        {
+            _emailSettings.CC = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [TestCase("readarr")]
+        [TestCase("readarr@readarr")]
+        [TestCase("readarr.com")]
+        public void should_not_be_valid_if_bcc_is_invalid(string email)
+        {
+            _emailSettings.Bcc = new string[] { email };
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_be_valid_if_to_bcc_cc_are_all_empty()
+        {
+            _emailSettings.To = Array.Empty<string>();
+            _emailSettings.CC = Array.Empty<string>();
+            _emailSettings.Bcc = Array.Empty<string>();
+
+            _validator.Validate(_emailSettings).IsValid.Should().BeFalse();
+        }
+    }
+}

src/NzbDrone.Core/Notifications/Email/Email.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation.Results;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using NLog;
 using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Core.Notifications.Email
 {
     public class Email : NotificationBase<EmailSettings>
     {
-        private readonly IEmailService _emailService;
+        private readonly Logger _logger;
 
         public override string Name => "Email";
 
-        public Email(IEmailService emailService)
+        public Email(Logger logger)
         {
-            _emailService = emailService;
+            _logger = logger;
         }
 
         public override string Link => null;
@@ -21,7 +27,7 @@ namespace NzbDrone.Core.Notifications.Email
         {
             var body = $"{grabMessage.Message} sent to queue.";
 
-            _emailService.SendEmail(Settings, BOOK_GRABBED_TITLE_BRANDED, body);
+            SendEmail(Settings, BOOK_GRABBED_TITLE_BRANDED, body);
         }
 
         public override void OnReleaseImport(BookDownloadMessage message)
@@ -30,31 +36,144 @@ namespace NzbDrone.Core.Notifications.Email
 
             var paths = Settings.AttachFiles ? message.BookFiles.SelectList(a => a.Path) : null;
 
-            _emailService.SendEmail(Settings, BOOK_DOWNLOADED_TITLE_BRANDED, body, false, paths);
+            SendEmail(Settings, BOOK_DOWNLOADED_TITLE_BRANDED, body, paths);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck message)
         {
-            _emailService.SendEmail(Settings, HEALTH_ISSUE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, HEALTH_ISSUE_TITLE_BRANDED, message.Message);
         }
 
         public override void OnDownloadFailure(DownloadFailedMessage message)
         {
-            _emailService.SendEmail(Settings, DOWNLOAD_FAILURE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, DOWNLOAD_FAILURE_TITLE_BRANDED, message.Message);
         }
 
         public override void OnImportFailure(BookDownloadMessage message)
         {
-            _emailService.SendEmail(Settings, IMPORT_FAILURE_TITLE_BRANDED, message.Message);
+            SendEmail(Settings, IMPORT_FAILURE_TITLE_BRANDED, message.Message);
         }
 
         public override ValidationResult Test()
         {
             var failures = new List<ValidationFailure>();
 
-            failures.AddIfNotNull(_emailService.Test(Settings));
+            failures.AddIfNotNull(Test(Settings));
 
             return new ValidationResult(failures);
+        }
+
+        public ValidationFailure Test(EmailSettings settings)
+        {
+            const string body = "Success! You have properly configured your email notification settings";
+
+            try
+            {
+                SendEmail(settings, "Readarr - Test Notification", body);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test email");
+                return new ValidationFailure("Server", "Unable to send test email");
+            }
+
+            return null;
+        }
+
+        private void SendEmail(EmailSettings settings, string subject, string body, bool htmlBody = false, List<string> attachmentUrls = null)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(ParseAddress("From", settings.From));
+            email.To.AddRange(settings.To.Select(x => ParseAddress("To", x)));
+            email.Cc.AddRange(settings.CC.Select(x => ParseAddress("CC", x)));
+            email.Bcc.AddRange(settings.Bcc.Select(x => ParseAddress("BCC", x)));
+
+            email.Subject = subject;
+            email.Body = new TextPart(htmlBody ? "html" : "plain")
+            {
+                Text = body
+            };
+
+            _logger.Debug("Sending email Subject: {0}", subject);
+
+            if (attachmentUrls != null)
+            {
+                foreach (var url in attachmentUrls)
+                {
+                    email.Attachments.Add(new Attachment(url));
+                }
+            }
+
+            try
+            {
+                Send(email, settings);
+                _logger.Debug("Email sent. Subject: {0}", subject);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error sending email. Subject: {0}", email.Subject);
+                _logger.Debug(ex, ex.Message);
+                throw;
+            }
+
+            _logger.Debug("Finished sending email. Subject: {0}", email.Subject);
+        }
+
+        private void Send(MimeMessage email, EmailSettings settings)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Timeout = 10000;
+
+                var serverOption = SecureSocketOptions.Auto;
+
+                if (settings.RequireEncryption)
+                {
+                    if (settings.Port == 465)
+                    {
+                        serverOption = SecureSocketOptions.SslOnConnect;
+                    }
+                    else
+                    {
+                        serverOption = SecureSocketOptions.StartTls;
+                    }
+                }
+
+                _logger.Debug("Connecting to mail server");
+
+                client.Connect(settings.Server, settings.Port, serverOption);
+
+                if (!string.IsNullOrWhiteSpace(settings.Username))
+                {
+                    _logger.Debug("Authenticating to mail server");
+
+                    client.Authenticate(settings.Username, settings.Password);
+                }
+
+                _logger.Debug("Sending to mail server");
+
+                client.Send(email);
+
+                _logger.Debug("Sent to mail server, disconnecting");
+
+                client.Disconnect(true);
+
+                _logger.Debug("Disconnecting from mail server");
+            }
+        }
+
+        private MailboxAddress ParseAddress(string type, string address)
+        {
+            try
+            {
+                return MailboxAddress.Parse(address);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "{0} email address '{1}' invalid", type, address);
+                throw;
+            }
         }
     }
 }