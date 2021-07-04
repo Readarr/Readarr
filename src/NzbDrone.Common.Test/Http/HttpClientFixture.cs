Sonarr/Sonarr

Fixed: Qbittorrent api errors when only one of two seed criteria was configured
closes #4393

src/NzbDrone.Common.Test/Http/HttpClientFixture.cs
@@ -180,6 +180,38 @@ namespace NzbDrone.Common.Test.Http
         }
 
         [Test]
+        public void should_not_throw_on_suppressed_status_codes()
+        {
+            var request = new HttpRequest($"http://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+            request.SuppressHttpErrorStatusCodes = new[] { HttpStatusCode.NotFound };
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.IgnoreWarns();
+        }
+
+        [Test]
+        public void should_log_unsuccessful_status_codes()
+        {
+            var request = new HttpRequest($"http://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.ExpectedWarns(1);
+        }
+
+        [Test]
+        public void should_not_log_unsuccessful_status_codes()
+        {
+            var request = new HttpRequest($"http://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+            request.LogHttpError = false;
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.ExpectedWarns(0);
+        }
+
+        [Test]
         public void should_not_follow_redirects_when_not_in_production()
         {
             var request = new HttpRequest($"http://{_httpBinHost}/redirect/1");

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -88,7 +88,14 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
                 }
 
                 if (moveToTop)
@@ -139,8 +146,15 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
-                }
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
+            }
 
                 if (moveToTop)
                 {
@@ -172,14 +186,16 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
         protected bool WaitForTorrent(string hash)
         {
-            var count = 5;
+            var count = 10;
 
             while (count != 0)
             {
                 try
                 {
-                    Proxy.GetTorrentProperties(hash.ToLower(), Settings);
-                    return true;
+                    if (Proxy.IsTorrentLoaded(hash.ToLower(), Settings))
+                    {
+                        return true;
+                    }
                 }
                 catch
                 {

Readarr/Readarr

closes #907
(cherry picked from commit 652d44722b96a7a830ec45fe83260ddcecc525a7)

src/NzbDrone.Common.Test/Http/HttpClientFixture.cs
@@ -186,6 +186,38 @@ namespace NzbDrone.Common.Test.Http
         }
 
         [Test]
+        public void should_not_throw_on_suppressed_status_codes()
+        {
+            var request = new HttpRequest($"https://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+            request.SuppressHttpErrorStatusCodes = new[] { HttpStatusCode.NotFound };
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.IgnoreWarns();
+        }
+
+        [Test]
+        public void should_log_unsuccessful_status_codes()
+        {
+            var request = new HttpRequest($"https://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.ExpectedWarns(1);
+        }
+
+        [Test]
+        public void should_not_log_unsuccessful_status_codes()
+        {
+            var request = new HttpRequest($"https://{_httpBinHost}/status/{HttpStatusCode.NotFound}");
+            request.LogHttpError = false;
+
+            var exception = Assert.Throws<HttpException>(() => Subject.Get<HttpBinResource>(request));
+
+            ExceptionVerification.ExpectedWarns(0);
+        }
+
+        [Test]
         public void should_not_follow_redirects_when_not_in_production()
         {
             var request = new HttpRequest($"https://{_httpBinHost}/redirect/1");

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -88,7 +88,14 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
                 }
 
                 if (moveToTop)
@@ -138,8 +145,15 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
-                }
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
+            }
 
                 if (moveToTop)
                 {
@@ -171,14 +185,16 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
         protected bool WaitForTorrent(string hash)
         {
-            var count = 5;
+            var count = 10;
 
             while (count != 0)
             {
                 try
                 {
-                    Proxy.GetTorrentProperties(hash.ToLower(), Settings);
-                    return true;
+                    if (Proxy.IsTorrentLoaded(hash.ToLower(), Settings))
+                    {
+                        return true;
+                    }
                 }
                 catch
                 {