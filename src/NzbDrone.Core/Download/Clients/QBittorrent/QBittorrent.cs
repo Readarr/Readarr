Sonarr/Sonarr

Fixed: Qbittorrent api errors when only one of two seed criteria was configured
closes #4393

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -88,7 +88,14 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
                 }
 
                 if (moveToTop)
@@ -139,8 +146,15 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
-                }
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteEpisode.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
+            }
 
                 if (moveToTop)
                 {
@@ -172,14 +186,16 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
         protected bool WaitForTorrent(string hash)
         {
-            var count = 5;
+            var count = 10;
 
             while (count != 0)
             {
                 try
                 {
-                    Proxy.GetTorrentProperties(hash.ToLower(), Settings);
-                    return true;
+                    if (Proxy.IsTorrentLoaded(hash.ToLower(), Settings))
+                    {
+                        return true;
+                    }
                 }
                 catch
                 {

Readarr/Readarr

closes #907
(cherry picked from commit 652d44722b96a7a830ec45fe83260ddcecc525a7)

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -89,7 +89,14 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
                 }
 
                 if (moveToTop)
@@ -140,8 +147,15 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
                 if (!addHasSetShareLimits && setShareLimits)
                 {
-                    Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
-                }
+                    try
+                    {
+                        Proxy.SetTorrentSeedingConfiguration(hash.ToLower(), remoteBook.SeedConfiguration, Settings);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Failed to set the torrent seed criteria for {0}.", hash);
+                    }
+            }
 
                 if (moveToTop)
                 {
@@ -173,14 +187,16 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
         protected bool WaitForTorrent(string hash)
         {
-            var count = 5;
+            var count = 10;
 
             while (count != 0)
             {
                 try
                 {
-                    Proxy.GetTorrentProperties(hash.ToLower(), Settings);
-                    return true;
+                    if (Proxy.IsTorrentLoaded(hash.ToLower(), Settings))
+                    {
+                        return true;
+                    }
                 }
                 catch
                 {