Radarr/Radarr

Improve default path for Synology Download Station
Fixes: Missing default path for Download Station
Fixes: Error when getting destination path for Synology Download Station in health check

(cherry picked from commit 9638a6591bcb6db312150f9c8cfc9aaed86f0c56)

src/NzbDrone.Core/Download/Clients/DownloadStation/TorrentDownloadStation.cs
@@ -451,14 +451,15 @@ namespace NzbDrone.Core.Download.Clients.DownloadStation
             {
                 return Settings.TvDirectory.TrimStart('/');
             }
-            else if (Settings.TvCategory.IsNotNullOrWhiteSpace())
-            {
-                var destDir = GetDefaultDir();
+ 
+            var destDir = GetDefaultDir();
 
+            if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+            {
                 return $"{destDir.TrimEnd('/')}/{Settings.TvCategory}";
             }
 
-            return null;
+            return destDir.TrimEnd('/');
         }
     }
 }

src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
@@ -427,14 +427,15 @@ namespace NzbDrone.Core.Download.Clients.DownloadStation
             {
                 return Settings.TvDirectory.TrimStart('/');
             }
-            else if (Settings.TvCategory.IsNotNullOrWhiteSpace())
-            {
-                var destDir = GetDefaultDir();
 
+            var destDir = GetDefaultDir();
+
+            if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+            {
                 return $"{destDir.TrimEnd('/')}/{Settings.TvCategory}";
             }
 
-            return null;
+            return destDir.TrimEnd('/');
         }
     }
 }

Readarr/Readarr

(cherry picked from commit 4bf3ab1511b4ea25642476bf9df13f91b6f73d76)

src/NzbDrone.Core/Download/Clients/DownloadStation/TorrentDownloadStation.cs
@@ -450,14 +450,15 @@ namespace NzbDrone.Core.Download.Clients.DownloadStation
             {
                 return Settings.TvDirectory.TrimStart('/');
             }
-            else if (Settings.MusicCategory.IsNotNullOrWhiteSpace())
-            {
-                var destDir = GetDefaultDir();
 
+            var destDir = GetDefaultDir();
+
+            if (Settings.MusicCategory.IsNotNullOrWhiteSpace())
+            {
                 return $"{destDir.TrimEnd('/')}/{Settings.MusicCategory}";
             }
 
-            return null;
+            return destDir.TrimEnd('/');
         }
     }
 }

src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
@@ -426,14 +426,15 @@ namespace NzbDrone.Core.Download.Clients.DownloadStation
             {
                 return Settings.TvDirectory.TrimStart('/');
             }
-            else if (Settings.MusicCategory.IsNotNullOrWhiteSpace())
-            {
-                var destDir = GetDefaultDir();
 
+            var destDir = GetDefaultDir();
+
+            if (Settings.MusicCategory.IsNotNullOrWhiteSpace())
+            {
                 return $"{destDir.TrimEnd('/')}/{Settings.MusicCategory}";
             }
 
-            return null;
+            return destDir.TrimEnd('/');
         }
     }
 }