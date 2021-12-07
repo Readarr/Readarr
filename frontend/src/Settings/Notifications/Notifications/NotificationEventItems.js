91b53b5301c4fd76d7502e6da558ab156c502953
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-12-07 16:32:54 -0600

    Fixed: Various Wiki Links

--- frontend/src/Settings/Notifications/Notifications/NotificationEventItems.js
+++ frontend/src/Settings/Notifications/Notifications/NotificationEventItems.js
@@ -42,7 +42,7 @@ function NotificationEventItems(props) {
       <div>
         <FormInputHelpText
           text="Select which events should trigger this notification"
-          link="https://wiki.servarr.com/readarr/settings#Connections"
+          link="https://wiki.servarr.com/readarr/settings#connections"
         />
         <div className={styles.events}>
           <div>
