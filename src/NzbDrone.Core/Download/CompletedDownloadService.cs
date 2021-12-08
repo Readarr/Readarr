Sonarr/Sonarr

Fixed: Error when processing release with files Sonarr is unable to parse

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -127,7 +127,7 @@ namespace NzbDrone.Core.Download
             if (importResults.Any(c => c.Result != ImportResultType.Imported))
             {
                 var statusMessages = importResults
-                    .Where(v => v.Result != ImportResultType.Imported)
+                    .Where(v => v.Result != ImportResultType.Imported && v.ImportDecision.LocalEpisode != null)
                     .Select(v => new TrackedDownloadStatusMessage(Path.GetFileName(v.ImportDecision.LocalEpisode.Path), v.Errors))
                     .ToArray();
 

Readarr/Readarr

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -115,7 +115,7 @@ namespace NzbDrone.Core.Download
             {
                 trackedDownload.State = TrackedDownloadState.ImportFailed;
                 var statusMessages = importResults
-                    .Where(v => v.Result != ImportResultType.Imported)
+                    .Where(v => v.Result != ImportResultType.Imported && v.ImportDecision.Item != null)
                     .Select(v => new TrackedDownloadStatusMessage(Path.GetFileName(v.ImportDecision.Item.Path), v.Errors))
                     .ToArray();
 