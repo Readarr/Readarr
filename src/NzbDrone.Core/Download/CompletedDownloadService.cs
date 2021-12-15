Sonarr/Sonarr

Additional logging when trying to complete tracked downloads

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NLog;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Download.TrackedDownloads;
@@ -29,13 +30,15 @@ namespace NzbDrone.Core.Download
         private readonly IParsingService _parsingService;
         private readonly ISeriesService _seriesService;
         private readonly ITrackedDownloadAlreadyImported _trackedDownloadAlreadyImported;
+        private readonly Logger _logger;
 
         public CompletedDownloadService(IEventAggregator eventAggregator,
                                         IHistoryService historyService,
                                         IDownloadedEpisodesImportService downloadedEpisodesImportService,
                                         IParsingService parsingService,
                                         ISeriesService seriesService,
-                                        ITrackedDownloadAlreadyImported trackedDownloadAlreadyImported)
+                                        ITrackedDownloadAlreadyImported trackedDownloadAlreadyImported,
+                                        Logger logger)
         {
             _eventAggregator = eventAggregator;
             _historyService = historyService;
@@ -140,6 +143,7 @@ namespace NzbDrone.Core.Download
 
             if (allEpisodesImported)
             {
+                _logger.Debug("All episodes were imported for {0}", trackedDownload.DownloadItem.Title);
                 trackedDownload.State = TrackedDownloadState.Imported;
                 _eventAggregator.PublishEvent(new DownloadCompletedEvent(trackedDownload, trackedDownload.RemoteEpisode.Series.Id));
                 return true;
@@ -164,12 +168,14 @@ namespace NzbDrone.Core.Download
 
                 if (allEpisodesImportedInHistory)
                 {
+                    _logger.Debug("All episodes were imported in history for {0}", trackedDownload.DownloadItem.Title);
                     trackedDownload.State = TrackedDownloadState.Imported;
                     _eventAggregator.PublishEvent(new DownloadCompletedEvent(trackedDownload, trackedDownload.RemoteEpisode.Series.Id));
                     return true;
                 }
             }
 
+            _logger.Debug("Not all episodes have been imported for {0}", trackedDownload.DownloadItem.Title);
             return false;
         }
     }

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadAlreadyImported.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.History;
 
@@ -12,10 +13,20 @@ namespace NzbDrone.Core.Download.TrackedDownloads
 
     public class TrackedDownloadAlreadyImported : ITrackedDownloadAlreadyImported
     {
+        private readonly Logger _logger;
+
+        public TrackedDownloadAlreadyImported(Logger logger)
+        {
+            _logger = logger;
+        }
+
         public bool IsImported(TrackedDownload trackedDownload, List<EpisodeHistory> historyItems)
         {
+            _logger.Trace("Checking if all episodes for '{0}' have been imported", trackedDownload.DownloadItem.Title);
+
             if (historyItems.Empty())
             {
+                _logger.Trace("No history for {0}", trackedDownload.DownloadItem.Title);
                 return false;
             }
 
@@ -25,11 +36,16 @@ namespace NzbDrone.Core.Download.TrackedDownloads
 
                 if (lastHistoryItem == null)
                 {
+                    _logger.Trace("No history for episode: S{0:00}E{1:00} [{2}]", e.SeasonNumber, e.EpisodeNumber, e.Id);
                     return false;
                 }
 
+                _logger.Trace("Last event for episode: S{0:00}E{1:00} [{2}] is: {3}", e.SeasonNumber, e.EpisodeNumber, e.Id, lastHistoryItem.EventType);
+
                 return lastHistoryItem.EventType == EpisodeHistoryEventType.DownloadFolderImported;
             });
+
+            _logger.Trace("All episodes for '{0}' have been imported: {1}", trackedDownload.DownloadItem.Title, allEpisodesImportedInHistory);
 
             return allEpisodesImportedInHistory;
         }

src/NzbDrone.Core/MediaFiles/EpisodeImport/Specifications/AlreadyImportedSpecification.cs
@@ -44,12 +44,14 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
 
                 if (lastImported == null)
                 {
+                    _logger.Trace("Episode file has not been imported");
                     continue;
                 }
 
                 // If the release was grabbed again after importing don't reject it
                 if (lastGrabbed != null && lastGrabbed.Date.After(lastImported.Date))
                 {
+                    _logger.Trace("Episode file was grabbed again after importing");
                     continue;
                 }
 

Readarr/Readarr

(cherry picked from commit 2df6b5370067635fdc9196451e09e4c2e6427eac)

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NLog;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Books;
@@ -29,13 +30,15 @@ namespace NzbDrone.Core.Download
         private readonly IAuthorService _authorService;
         private readonly IProvideImportItemService _importItemService;
         private readonly ITrackedDownloadAlreadyImported _trackedDownloadAlreadyImported;
+        private readonly Logger _logger;
 
         public CompletedDownloadService(IEventAggregator eventAggregator,
                                         IHistoryService historyService,
                                         IProvideImportItemService importItemService,
                                         IDownloadedBooksImportService downloadedTracksImportService,
                                         IAuthorService authorService,
-                                        ITrackedDownloadAlreadyImported trackedDownloadAlreadyImported)
+                                        ITrackedDownloadAlreadyImported trackedDownloadAlreadyImported,
+                                        Logger logger)
         {
             _eventAggregator = eventAggregator;
             _historyService = historyService;
@@ -43,6 +46,7 @@ namespace NzbDrone.Core.Download
             _downloadedTracksImportService = downloadedTracksImportService;
             _authorService = authorService;
             _trackedDownloadAlreadyImported = trackedDownloadAlreadyImported;
+            _logger = logger;
         }
 
         public void Check(TrackedDownload trackedDownload)
@@ -129,6 +133,7 @@ namespace NzbDrone.Core.Download
 
             if (allItemsImported)
             {
+                _logger.Debug("All books were imported for {0}", trackedDownload.DownloadItem.Title);
                 trackedDownload.State = TrackedDownloadState.Imported;
 
                 var importedAuthorId = importResults.Where(x => x.Result == ImportResultType.Imported)
@@ -156,6 +161,7 @@ namespace NzbDrone.Core.Download
 
                 if (allEpisodesImportedInHistory)
                 {
+                    _logger.Debug("All books were imported in history for {0}", trackedDownload.DownloadItem.Title);
                     trackedDownload.State = TrackedDownloadState.Imported;
 
                     var importedAuthorId = historyItems.Where(x => x.EventType == EntityHistoryEventType.BookFileImported)
@@ -166,6 +172,7 @@ namespace NzbDrone.Core.Download
                 }
             }
 
+            _logger.Debug("Not all books have been imported for {0}", trackedDownload.DownloadItem.Title);
             return false;
         }
     }

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadAlreadyImported.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.History;
 
@@ -12,10 +13,20 @@ namespace NzbDrone.Core.Download.TrackedDownloads
 
     public class TrackedDownloadAlreadyImported : ITrackedDownloadAlreadyImported
     {
+        private readonly Logger _logger;
+
+        public TrackedDownloadAlreadyImported(Logger logger)
+        {
+            _logger = logger;
+        }
+
         public bool IsImported(TrackedDownload trackedDownload, List<EntityHistory> historyItems)
         {
+            _logger.Trace("Checking if all books for '{0}' have been imported", trackedDownload.DownloadItem.Title);
+
             if (historyItems.Empty())
             {
+                _logger.Trace("No history for {0}", trackedDownload.DownloadItem.Title);
                 return false;
             }
 
@@ -30,11 +41,16 @@ namespace NzbDrone.Core.Download.TrackedDownloads
 
                 if (lastHistoryItem == null)
                 {
+                    _logger.Trace($"No history for book: {book}");
                     return false;
                 }
 
+                _logger.Trace($"Last event for book: {book} is: {lastHistoryItem.EventType}");
+
                 return lastHistoryItem.EventType == EntityHistoryEventType.BookFileImported;
             });
+
+            _logger.Trace("All books for '{0}' have been imported: {1}", trackedDownload.DownloadItem.Title, allBooksImportedInHistory);
 
             return allBooksImportedInHistory;
         }

src/NzbDrone.Core/MediaFiles/BookImport/Specifications/AlreadyImportedSpecification.cs
@@ -44,11 +44,13 @@ namespace NzbDrone.Core.MediaFiles.BookImport.Specifications
 
             if (lastImported == null)
             {
+                _logger.Trace("Book file has not been imported");
                 return Decision.Accept();
             }
 
             if (lastGrabbed != null && lastGrabbed.Date.After(lastImported.Date))
             {
+                _logger.Trace("Book file was grabbed again after importing");
                 return Decision.Accept();
             }
 