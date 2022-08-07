117a5c8010a75a777b5a5028f52dfb43817a2e1d
Devin Buhl <onedr0p@users.noreply.github.com>
2022-08-07 14:54:39 -0400

    New: Add application URL to host configuration settings
    Fixes #1816
    Closes #1817
    
    (cherry picked from commit 762042ba97c2ae689cee32d8e66a458f6d7a8adc)

--- src/NzbDrone.Core/Configuration/IConfigService.cs
+++ src/NzbDrone.Core/Configuration/IConfigService.cs
@@ -97,5 +97,6 @@ namespace NzbDrone.Core.Configuration
         int BackupRetention { get; }
 
         CertificateValidationType CertificateValidation { get; }
+        string ApplicationUrl { get; }
     }
 }
