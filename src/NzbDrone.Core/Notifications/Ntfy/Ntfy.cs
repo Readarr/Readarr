eafaba355a01cff2896be6fd88288b3ff9c709ee
Chris <1967906+psylenced@users.noreply.github.com>
2022-09-23 02:46:26 +1000

    Added: Ntfy provider for notifications
    (cherry picked from commit f6e6bc98f7846328d158ab2f804ca65b49632912)

---
+++ src/NzbDrone.Core/Notifications/Ntfy/Ntfy.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public class Ntfy : NotificationBase<NtfySettings>
+    {
+        private readonly INtfyProxy _proxy;
+
+        public Ntfy(INtfyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Name => "ntfy.sh";
+
+        public override string Link => "https://ntfy.sh/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(BOOK_GRABBED_TITLE_BRANDED, grabMessage.Message, Settings);
+        }
+
+        public override void OnReleaseImport(BookDownloadMessage message)
+        {
+            _proxy.SendNotification(BOOK_DOWNLOADED_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnAuthorDelete(AuthorDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(AUTHOR_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnBookDelete(BookDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(BOOK_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnBookFileDelete(BookFileDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(BOOK_FILE_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE_BRANDED, healthCheck.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}
