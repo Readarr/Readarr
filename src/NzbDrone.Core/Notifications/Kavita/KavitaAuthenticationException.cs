3f5d022277aa20b7da1beeebadae1f99ae9465d8
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-10-01 19:33:27 -0500

    Added ability for Readarr to inform Kavita when a change occurs for rescan.

---
+++ src/NzbDrone.Core/Notifications/Kavita/KavitaAuthenticationException.cs
@@ -0,0 +1,14 @@
+﻿namespace NzbDrone.Core.Notifications.Kavita;
+
+public class KavitaAuthenticationException : KavitaException
+{
+    public KavitaAuthenticationException(string message)
+        : base(message)
+    {
+    }
+
+    public KavitaAuthenticationException(string message, params object[] args)
+        : base(message, args)
+    {
+    }
+}
---
+++ src/NzbDrone.Core/Notifications/Kavita/KavitaService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FluentValidation.Results;
+using NLog;
+
+namespace NzbDrone.Core.Notifications.Kavita;
+
+public interface IKavitaService
+{
+    void Notify(KavitaSettings settings, string message);
+    ValidationFailure Test(KavitaSettings settings, string message);
+}
+
+public class KavitaService : IKavitaService
+{
+    private readonly IKavitaServiceProxy _proxy;
+    private readonly Logger _logger;
+
+    public KavitaService(IKavitaServiceProxy proxy,
+        Logger logger)
+    {
+        _proxy = proxy;
+        _logger = logger;
+    }
+
+    public void Notify(KavitaSettings settings, string folderPath)
+    {
+        _proxy.Notify(settings, folderPath);
+    }
+
+    private string GetToken(KavitaSettings settings)
+    {
+        return _proxy.GetToken(settings);
+    }
+
+    public ValidationFailure Test(KavitaSettings settings, string message)
+    {
+        try
+        {
+            _logger.Debug("Determining Authentication of Host: {0}", _proxy.GetBaseUrl(settings));
+            var token = GetToken(settings);
+            _logger.Debug("Token is: {0}", token);
+        }
+        catch (KavitaAuthenticationException ex)
+        {
+            _logger.Error(ex, "Unable to connect to Kavita Server");
+            return new ValidationFailure("ApiKey", "Incorrect ApiKey");
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Unable to connect to Kavita Server");
+            return new ValidationFailure("Host", "Unable to connect to Kavita Server");
+        }
+
+        return null;
+    }
+}
