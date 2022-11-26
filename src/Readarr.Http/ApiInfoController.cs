Sonarr/Sonarr

New: Base API info endpoint

src/Sonarr.Http/ApiInfoController.cs
@@ -1,0 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NzbDrone.Http
+{
+    public class ApiInfoController : Controller
+    {
+        public ApiInfoController()
+        {
+        }
+
+        [HttpGet("/api")]
+        [Produces("application/json")]
+        public object GetApiInfo()
+        {
+            return new ApiInfoResource
+            {
+                Current = "v3",
+                Deprecated = new List<string>()
+            };
+        }
+    }
+}

src/Sonarr.Http/ApiInfoResource.cs
@@ -1,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NzbDrone.Http
+{
+    public class ApiInfoResource
+    {
+        public string Current { get; set; }
+        public List<string> Deprecated { get; set; }
+    }
+}

Readarr/Readarr

(cherry picked from commit 5e57ffbcf9ac3a346d4bf2b692248393215bad89)

src/Readarr.Http/ApiInfoController.cs
@@ -1,0 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Readarr.Http
+{
+    public class ApiInfoController : Controller
+    {
+        public ApiInfoController()
+        {
+        }
+
+        [HttpGet("/api")]
+        [Produces("application/json")]
+        public ApiInfoResource GetApiInfo()
+        {
+            return new ApiInfoResource
+            {
+                Current = "v1",
+                Deprecated = new List<string>()
+            };
+        }
+    }
+}

src/Readarr.Http/ApiInfoResource.cs
@@ -1,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readarr.Http
+{
+    public class ApiInfoResource
+    {
+        public string Current { get; set; }
+        public List<string> Deprecated { get; set; }
+    }
+}