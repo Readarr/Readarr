Lidarr/Lidarr

Fixed: Manual Import Reprocessing

frontend/src/Store/Actions/interactiveImportActions.js
@@ -5,10 +5,9 @@ import { sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import updateSectionState from 'Utilities/State/updateSectionState';
-import { set, update } from './baseActions';
+import { set, update, updateItem } from './baseActions';
 import createFetchHandler from './Creators/createFetchHandler';
 import createHandleActions from './Creators/createHandleActions';
-import createSaveProviderHandler from './Creators/createSaveProviderHandler';
 import createSetClientSideCollectionSortReducer from './Creators/Reducers/createSetClientSideCollectionSortReducer';
 
 //
@@ -18,6 +17,8 @@ export const section = 'interactiveImport';
 
 const albumsSection = `${section}.albums`;
 const trackFilesSection = `${section}.trackFiles`;
+let abortCurrentRequest = null;
+let currentIds = [];
 
 const MAXIMUM_RECENT_FOLDERS = 10;
 
@@ -34,7 +35,7 @@ export const defaultState = {
   sortKey: 'path',
   sortDirection: sortDirections.ASCENDING,
   recentFolders: [],
-  importMode: 'move',
+  importMode: 'chooseImportMode',
   sortPredicates: {
     path: function(item, direction) {
       const path = item.path;
@@ -156,7 +157,83 @@ export const actionHandlers = handleThunks({
     });
   },
 
-  [SAVE_INTERACTIVE_IMPORT_ITEM]: createSaveProviderHandler(section, '/manualimport', {}, true),
+  [SAVE_INTERACTIVE_IMPORT_ITEM]: function(getState, payload, dispatch) {
+    if (abortCurrentRequest) {
+      abortCurrentRequest();
+    }
+
+    dispatch(batchActions([
+      ...currentIds.map((id) => updateItem({
+        section,
+        id,
+        isReprocessing: false,
+        updateOnly: true
+      })),
+      ...payload.ids.map((id) => updateItem({
+        section,
+        id,
+        isReprocessing: true,
+        updateOnly: true
+      }))
+    ]));
+
+    const items = getState()[section].items;
+
+    const requestPayload = payload.ids.map((id) => {
+      const item = items.find((i) => i.id === id);
+
+      return {
+        id,
+        path: item.path,
+        artistId: item.artist ? item.artist.id : undefined,
+        albumId: item.album ? item.album.id : undefined,
+        albumReleaseId: item.albumReleaseId ? item.albumReleaseId : undefined,
+        trackIds: (item.tracks || []).map((e) => e.id),
+        quality: item.quality,
+        releaseGroup: item.releaseGroup,
+        downloadId: item.downloadId,
+        additionalFile: item.additionalFile,
+        replaceExistingFiles: item.replaceExistingFiles,
+        disableReleaseSwitching: item.disableReleaseSwitching
+      };
+    });
+
+    const { request, abortRequest } = createAjaxRequest({
+      method: 'POST',
+      url: '/manualimport',
+      contentType: 'application/json',
+      data: JSON.stringify(requestPayload)
+    });
+
+    abortCurrentRequest = abortRequest;
+    currentIds = payload.ids;
+
+    request.done((data) => {
+      dispatch(batchActions(
+        data.map((item) => updateItem({
+          section,
+          ...item,
+          isReprocessing: false,
+          updateOnly: true
+        }))
+      ));
+    });
+
+    request.fail((xhr) => {
+      if (xhr.aborted) {
+        return;
+      }
+
+      dispatch(batchActions(
+        payload.ids.map((id) => updateItem({
+          section,
+          id,
+          isReprocessing: false,
+          updateOnly: true
+        }))
+      ));
+    });
+  },
 
   [FETCH_INTERACTIVE_IMPORT_ALBUMS]: createFetchHandler(albumsSection, '/album'),
 

src/Lidarr.Api.V1/ManualImport/ManualImportController.cs
@@ -32,8 +32,8 @@ namespace Lidarr.Api.V1.ManualImport
             _logger = logger;
         }
 
-        [HttpPut]
-        public IActionResult UpdateItems(List<ManualImportResource> resource)
+        [HttpPost]
+        public IActionResult UpdateItems(List<ManualImportUpdateResource> resource)
         {
             return Accepted(UpdateImportItems(resource));
         }
@@ -65,7 +65,7 @@ namespace Lidarr.Api.V1.ManualImport
             return item;
         }
 
-        private List<ManualImportResource> UpdateImportItems(List<ManualImportResource> resources)
+        private List<ManualImportResource> UpdateImportItems(List<ManualImportUpdateResource> resources)
         {
             var items = new List<ManualImportItem>();
             foreach (var resource in resources)
@@ -75,11 +75,11 @@ namespace Lidarr.Api.V1.ManualImport
                     Id = resource.Id,
                     Path = resource.Path,
                     Name = resource.Name,
-                    Size = resource.Size,
-                    Artist = resource.Artist == null ? null : _artistService.GetArtist(resource.Artist.Id),
-                    Album = resource.Album == null ? null : _albumService.GetAlbum(resource.Album.Id),
-                    Release = resource.AlbumReleaseId == 0 ? null : _releaseService.GetRelease(resource.AlbumReleaseId),
+                    Artist = resource.ArtistId.HasValue ? _artistService.GetArtist(resource.ArtistId.Value) : null,
+                    Album = resource.AlbumId.HasValue ? _albumService.GetAlbum(resource.AlbumId.Value) : null,
+                    Release = resource.AlbumReleaseId.HasValue ? _releaseService.GetRelease(resource.AlbumReleaseId.Value) : null,
                     Quality = resource.Quality,
+                    ReleaseGroup = resource.ReleaseGroup,
                     DownloadId = resource.DownloadId,
                     AdditionalFile = resource.AdditionalFile,
                     ReplaceExistingFiles = resource.ReplaceExistingFiles,

Readarr/Readarr

frontend/src/Store/Actions/interactiveImportActions.js
@@ -5,10 +5,9 @@ import { sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import updateSectionState from 'Utilities/State/updateSectionState';
-import { set, update } from './baseActions';
+import { set, update, updateItem } from './baseActions';
 import createFetchHandler from './Creators/createFetchHandler';
 import createHandleActions from './Creators/createHandleActions';
-import createSaveProviderHandler from './Creators/createSaveProviderHandler';
 import createSetClientSideCollectionSortReducer from './Creators/Reducers/createSetClientSideCollectionSortReducer';
 
 //
@@ -18,6 +17,8 @@ export const section = 'interactiveImport';
 
 const booksSection = `${section}.books`;
 const bookFilesSection = `${section}.bookFiles`;
+let abortCurrentRequest = null;
+let currentIds = [];
 
 const MAXIMUM_RECENT_FOLDERS = 10;
 
@@ -34,7 +35,7 @@ export const defaultState = {
   sortKey: 'quality',
   sortDirection: sortDirections.DESCENDING,
   recentFolders: [],
-  importMode: 'move',
+  importMode: 'chooseImportMode',
   sortPredicates: {
     path: function(item, direction) {
       const path = item.path;
@@ -156,7 +157,82 @@ export const actionHandlers = handleThunks({
     });
   },
 
-  [SAVE_INTERACTIVE_IMPORT_ITEM]: createSaveProviderHandler(section, '/manualimport', {}, true),
+  [SAVE_INTERACTIVE_IMPORT_ITEM]: function(getState, payload, dispatch) {
+    if (abortCurrentRequest) {
+      abortCurrentRequest();
+    }
+
+    dispatch(batchActions([
+      ...currentIds.map((id) => updateItem({
+        section,
+        id,
+        isReprocessing: false,
+        updateOnly: true
+      })),
+      ...payload.ids.map((id) => updateItem({
+        section,
+        id,
+        isReprocessing: true,
+        updateOnly: true
+      }))
+    ]));
+
+    const items = getState()[section].items;
+
+    const requestPayload = payload.ids.map((id) => {
+      const item = items.find((i) => i.id === id);
+
+      return {
+        id,
+        path: item.path,
+        authorId: item.author ? item.author.id : undefined,
+        bookId: item.book ? item.book.id : undefined,
+        foreignEditionId: item.foreignEditionId ? item.ForeignEditionId : undefined,
+        quality: item.quality,
+        releaseGroup: item.releaseGroup,
+        downloadId: item.downloadId,
+        additionalFile: item.additionalFile,
+        replaceExistingFiles: item.replaceExistingFiles,
+        disableReleaseSwitching: item.disableReleaseSwitching
+      };
+    });
+
+    const { request, abortRequest } = createAjaxRequest({
+      method: 'POST',
+      url: '/manualimport',
+      contentType: 'application/json',
+      data: JSON.stringify(requestPayload)
+    });
+
+    abortCurrentRequest = abortRequest;
+    currentIds = payload.ids;
+
+    request.done((data) => {
+      dispatch(batchActions(
+        data.map((item) => updateItem({
+          section,
+          ...item,
+          isReprocessing: false,
+          updateOnly: true
+        }))
+      ));
+    });
+
+    request.fail((xhr) => {
+      if (xhr.aborted) {
+        return;
+      }
+
+      dispatch(batchActions(
+        payload.ids.map((id) => updateItem({
+          section,
+          id,
+          isReprocessing: false,
+          updateOnly: true
+        }))
+      ));
+    });
+  },
 
   [FETCH_INTERACTIVE_IMPORT_BOOKS]: createFetchHandler(booksSection, '/book'),
 

src/Readarr.Api.V1/ManualImport/ManualImportController.cs
@@ -32,8 +32,8 @@ namespace Readarr.Api.V1.ManualImport
             _logger = logger;
         }
 
-        [HttpPut]
-        public IActionResult UpdateItems(List<ManualImportResource> resource)
+        [HttpPost]
+        public IActionResult UpdateItems(List<ManualImportUpdateResource> resource)
         {
             return Accepted(UpdateImportItems(resource));
         }
@@ -65,7 +65,7 @@ namespace Readarr.Api.V1.ManualImport
             return item;
         }
 
-        private List<ManualImportResource> UpdateImportItems(List<ManualImportResource> resources)
+        private List<ManualImportResource> UpdateImportItems(List<ManualImportUpdateResource> resources)
         {
             var items = new List<ManualImportItem>();
             foreach (var resource in resources)
@@ -75,11 +75,11 @@ namespace Readarr.Api.V1.ManualImport
                     Id = resource.Id,
                     Path = resource.Path,
                     Name = resource.Name,
-                    Size = resource.Size,
-                    Author = resource.Author == null ? null : _authorService.GetAuthor(resource.Author.Id),
-                    Book = resource.Book == null ? null : _bookService.GetBook(resource.Book.Id),
+                    Author = resource.AuthorId.HasValue ? _authorService.GetAuthor(resource.AuthorId.Value) : null,
+                    Book = resource.BookId.HasValue ? _bookService.GetBook(resource.BookId.Value) : null,
                     Edition = resource.ForeignEditionId == null ? null : _editionService.GetEditionByForeignEditionId(resource.ForeignEditionId),
                     Quality = resource.Quality,
+                    ReleaseGroup = resource.ReleaseGroup,
                     DownloadId = resource.DownloadId,
                     AdditionalFile = resource.AdditionalFile,
                     ReplaceExistingFiles = resource.ReplaceExistingFiles,