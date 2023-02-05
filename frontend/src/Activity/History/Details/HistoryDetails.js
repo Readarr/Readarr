4d3e83ec153e9fbeea3cdffb06389c447dd9c7cf
Qstick <qstick@gmail.com>
2023-02-04 20:40:41 -0600

    Fixed: Download client name in history details
    Closes #1413

--- frontend/src/Activity/History/Details/HistoryDetails.js
+++ frontend/src/Activity/History/Details/HistoryDetails.js
@@ -69,12 +69,15 @@ function HistoryDetails(props) {
       customFormatScore,
       nzbInfoUrl,
       downloadClient,
+      downloadClientName,
       downloadId,
       age,
       ageHours,
       ageMinutes,
       publishedDate
     } = data;
+
+    const downloadClientNameInfo = downloadClientName ?? downloadClient;
 
     return (
       <DescriptionList>
@@ -125,11 +128,12 @@ function HistoryDetails(props) {
         }
 
         {
-          !!downloadClient &&
+          downloadClientNameInfo ?
             <DescriptionListItem
               title={translate('DownloadClient')}
-              data={downloadClient}
-            />
+              data={downloadClientNameInfo}
+            /> :
+            null
         }
 
         {
