dcc2a14c602e0d29d02c37dd2adbf4f80cad3da5
Qstick <qstick@gmail.com>
2023-02-25 13:08:13 -0600

    Only send monitored edition in webhook payload.

--- src/NzbDrone.Core/Notifications/Webhook/WebhookBook.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookBook.cs
@@ -9,7 +9,6 @@ namespace NzbDrone.Core.Notifications.Webhook
     {
         public WebhookBook()
         {
-            Editions = new List<WebhookBookEdition>();
         }
 
         public WebhookBook(Book book)
@@ -18,13 +17,13 @@ namespace NzbDrone.Core.Notifications.Webhook
             GoodreadsId = book.ForeignBookId;
             Title = book.Title;
             ReleaseDate = book.ReleaseDate;
-            Editions = book.Editions.Value.Select(x => new WebhookBookEdition(x)).ToList();
+            Edition = new WebhookBookEdition(book.Editions.Value.Single(e => e.Monitored));
         }
 
         public int Id { get; set; }
         public string GoodreadsId { get; set; }
         public string Title { get; set; }
-        public List<WebhookBookEdition> Editions { get; set; }
+        public WebhookBookEdition Edition { get; set; }
         public DateTime? ReleaseDate { get; set; }
     }
 }
