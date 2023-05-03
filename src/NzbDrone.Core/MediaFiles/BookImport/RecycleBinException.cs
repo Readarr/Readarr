Sonarr/Sonarr

New: Report health error if Recycling Bin folder is not writable
Closes #4692

src/NzbDrone.Core/MediaFiles/EpisodeImport/RecycleBinException.cs
@@ -1,0 +1,28 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace NzbDrone.Core.MediaFiles.EpisodeImport
+{
+    public class RecycleBinException : DirectoryNotFoundException
+    {
+        public RecycleBinException()
+        {
+        }
+
+        public RecycleBinException(string message)
+            : base(message)
+        {
+        }
+
+        public RecycleBinException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected RecycleBinException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}

Readarr/Readarr

(cherry picked from commit 8c50cd061e691914d9fcce119b9f838f1276950c)

src/NzbDrone.Core/MediaFiles/BookImport/RecycleBinException.cs
@@ -1,0 +1,28 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace NzbDrone.Core.MediaFiles.BookImport
+{
+    public class RecycleBinException : DirectoryNotFoundException
+    {
+        public RecycleBinException()
+        {
+        }
+
+        public RecycleBinException(string message)
+            : base(message)
+        {
+        }
+
+        public RecycleBinException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected RecycleBinException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}