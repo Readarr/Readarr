Sonarr/Sonarr

New: Add tag support to indexers
Closes #487

frontend/src/Settings/Tags/Details/TagDetailsModalContent.js
@@ -21,6 +21,7 @@ function TagDetailsModalContent(props) {
     importLists,
     notifications,
     releaseProfiles,
+    indexers,
     onModalClose,
     onDeleteTagPress
   } = props;
@@ -38,7 +39,7 @@ function TagDetailsModalContent(props) {
         }
 
         {
-          !!series.length &&
+          series.length ?
             <FieldSet legend="Series">
               {
                 series.map((item) => {
@@ -49,11 +50,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!delayProfiles.length &&
+          delayProfiles.length ?
             <FieldSet legend="Delay Profile">
               {
                 delayProfiles.map((item) => {
@@ -78,11 +80,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!notifications.length &&
+          notifications.length ?
             <FieldSet legend="Connections">
               {
                 notifications.map((item) => {
@@ -93,11 +96,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!importLists.length &&
+          importLists.length ?
             <FieldSet legend="Import Lists">
               {
                 importLists.map((item) => {
@@ -108,11 +112,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!releaseProfiles.length &&
+          releaseProfiles.length ?
             <FieldSet legend="Release Profiles">
               {
                 releaseProfiles.map((item) => {
@@ -154,7 +159,24 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
+        }
+
+        {
+          indexers.length ?
+            <FieldSet legend="Indexers">
+              {
+                indexers.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      {item.name}
+                    </div>
+                  );
+                })
+              }
+            </FieldSet> :
+            null
         }
       </ModalBody>
 
@@ -189,6 +211,7 @@ TagDetailsModalContent.propTypes = {
   importLists: PropTypes.arrayOf(PropTypes.object).isRequired,
   notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
   releaseProfiles: PropTypes.arrayOf(PropTypes.object).isRequired,
+  indexers: PropTypes.arrayOf(PropTypes.object).isRequired,
   onModalClose: PropTypes.func.isRequired,
   onDeleteTagPress: PropTypes.func.isRequired
 };

frontend/src/Settings/Tags/Details/TagDetailsModalContentConnector.js
@@ -69,6 +69,14 @@ function createMatchingReleaseProfilesSelector() {
   );
 }
 
+function createMatchingIndexersSelector() {
+  return createSelector(
+    (state, { indexerIds }) => indexerIds,
+    (state) => state.settings.indexers.items,
+    findMatchingItems
+  );
+}
+
 function createMapStateToProps() {
   return createSelector(
     createMatchingSeriesSelector(),
@@ -76,13 +84,15 @@ function createMapStateToProps() {
     createMatchingImportListsSelector(),
     createMatchingNotificationsSelector(),
     createMatchingReleaseProfilesSelector(),
-    (series, delayProfiles, importLists, notifications, releaseProfiles) => {
+    createMatchingIndexersSelector(),
+    (series, delayProfiles, importLists, notifications, releaseProfiles, indexers) => {
       return {
         series,
         delayProfiles,
         importLists,
         notifications,
-        releaseProfiles
+        releaseProfiles,
+        indexers
       };
     }
   );

Readarr/Readarr

(cherry picked from commit c3d54b312ef18b837d54605ea78f1a263fd6900b)

frontend/src/Settings/Tags/Details/TagDetailsModalContent.js
@@ -21,6 +21,7 @@ function TagDetailsModalContent(props) {
     importLists,
     notifications,
     releaseProfiles,
+    indexers,
     onModalClose,
     onDeleteTagPress
   } = props;
@@ -40,7 +41,7 @@ function TagDetailsModalContent(props) {
         }
 
         {
-          !!author.length &&
+          author.length ?
             <FieldSet legend={translate('Authors')}>
               {
                 author.map((item) => {
@@ -51,11 +52,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!delayProfiles.length &&
+          delayProfiles.length ?
             <FieldSet legend={translate('DelayProfile')}>
               {
                 delayProfiles.map((item) => {
@@ -80,11 +82,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!notifications.length &&
+          notifications.length ?
             <FieldSet legend={translate('Connections')}>
               {
                 notifications.map((item) => {
@@ -95,11 +98,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!importLists.length &&
+          importLists.length ?
             <FieldSet legend={translate('ImportLists')}>
               {
                 importLists.map((item) => {
@@ -110,11 +114,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!releaseProfiles.length &&
+          releaseProfiles.length ?
             <FieldSet legend={translate('ReleaseProfiles')}>
               {
                 releaseProfiles.map((item) => {
@@ -150,13 +155,30 @@ function TagDetailsModalContent(props) {
                               </Label>
                             );
                           })
-                        }
+                        }s
                       </div>
                     </div>
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
+        }
+
+        {
+          indexers.length ?
+            <FieldSet legend={translate('Indexers')}>
+              {
+                indexers.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      {item.name}
+                    </div>
+                  );
+                })
+              }
+            </FieldSet> :
+            null
         }
       </ModalBody>
 
@@ -191,6 +213,7 @@ TagDetailsModalContent.propTypes = {
   importLists: PropTypes.arrayOf(PropTypes.object).isRequired,
   notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
   releaseProfiles: PropTypes.arrayOf(PropTypes.object).isRequired,
+  indexers: PropTypes.arrayOf(PropTypes.object).isRequired,
   onModalClose: PropTypes.func.isRequired,
   onDeleteTagPress: PropTypes.func.isRequired
 };

frontend/src/Settings/Tags/Details/TagDetailsModalContentConnector.js
@@ -69,6 +69,14 @@ function createMatchingReleaseProfilesSelector() {
   );
 }
 
+function createMatchingIndexersSelector() {
+  return createSelector(
+    (state, { indexerIds }) => indexerIds,
+    (state) => state.settings.indexers.items,
+    findMatchingItems
+  );
+}
+
 function createMapStateToProps() {
   return createSelector(
     createMatchingAuthorSelector(),
@@ -76,13 +84,15 @@ function createMapStateToProps() {
     createMatchingImportListsSelector(),
     createMatchingNotificationsSelector(),
     createMatchingReleaseProfilesSelector(),
-    (author, delayProfiles, importLists, notifications, releaseProfiles) => {
+    createMatchingIndexersSelector(),
+    (author, delayProfiles, importLists, notifications, releaseProfiles, indexers) => {
       return {
         author,
         delayProfiles,
         importLists,
         notifications,
-        releaseProfiles
+        releaseProfiles,
+        indexers
       };
     }
   );