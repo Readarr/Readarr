Sonarr/Sonarr

Use MinBy and MaxBy instead of OrderBy + First

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -10,7 +10,6 @@ using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Jobs;
-using NzbDrone.Core.Languages;
 using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Parser;
 using NzbDrone.Core.Parser.Model;
@@ -97,7 +96,7 @@ namespace NzbDrone.Core.Download.Pending
 
                     var episodeIds = decision.RemoteEpisode.Episodes.Select(e => e.Id);
 
-                    var existingReports = episodeIds.SelectMany(v => alreadyPendingByEpisode[v] ?? Enumerable.Empty<PendingRelease>())
+                    var existingReports = episodeIds.SelectMany(v => alreadyPendingByEpisode[v])
                                                     .Distinct().ToList();
 
                     var matchingReports = existingReports.Where(MatchingReleasePredicate(decision.RemoteEpisode.Release)).ToList();
@@ -243,8 +242,7 @@ namespace NzbDrone.Core.Download.Pending
 
             return seriesReleases.Select(r => r.RemoteEpisode)
                                  .Where(r => r.Episodes.Select(e => e.Id).Intersect(episodeIds).Any())
-                                 .OrderByDescending(p => p.Release.AgeHours)
-                                 .FirstOrDefault();
+                                 .MaxBy(p => p.Release.AgeHours);
         }
 
         private ILookup<int, PendingRelease> CreateEpisodeLookup(IEnumerable<PendingRelease> alreadyPending)

src/NzbDrone.Core/History/HistoryRepository.cs
@@ -30,9 +30,7 @@ namespace NzbDrone.Core.History
 
         public EpisodeHistory MostRecentForEpisode(int episodeId)
         {
-            return Query(h => h.EpisodeId == episodeId)
-                        .OrderByDescending(h => h.Date)
-                        .FirstOrDefault();
+            return Query(h => h.EpisodeId == episodeId).MaxBy(h => h.Date);
         }
 
         public List<EpisodeHistory> FindByEpisodeId(int episodeId)
@@ -44,9 +42,7 @@ namespace NzbDrone.Core.History
 
         public EpisodeHistory MostRecentForDownloadId(string downloadId)
         {
-            return Query(h => h.DownloadId == downloadId)
-             .OrderByDescending(h => h.Date)
-             .FirstOrDefault();
+            return Query(h => h.DownloadId == downloadId).MaxBy(h => h.Date);
         }
 
         public List<EpisodeHistory> FindByDownloadId(string downloadId)

Readarr/Readarr

(cherry picked from commit 6ea3d8c127eafbcf9d1b6e9338b737e91e256875)
Closes #2323

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -96,7 +96,7 @@ namespace NzbDrone.Core.Download.Pending
 
                     var bookIds = decision.RemoteBook.Books.Select(e => e.Id);
 
-                    var existingReports = bookIds.SelectMany(v => alreadyPendingByBook[v] ?? Enumerable.Empty<PendingRelease>())
+                    var existingReports = bookIds.SelectMany(v => alreadyPendingByBook[v])
                                                     .Distinct().ToList();
 
                     var matchingReports = existingReports.Where(MatchingReleasePredicate(decision.RemoteBook.Release)).ToList();
@@ -239,8 +239,7 @@ namespace NzbDrone.Core.Download.Pending
 
             return authorReleases.Select(r => r.RemoteBook)
                                  .Where(r => r.Books.Select(e => e.Id).Intersect(bookIds).Any())
-                                 .OrderByDescending(p => p.Release.AgeHours)
-                                 .FirstOrDefault();
+                                 .MaxBy(p => p.Release.AgeHours);
         }
 
         private ILookup<int, PendingRelease> CreateBookLookup(IEnumerable<PendingRelease> alreadyPending)

src/NzbDrone.Core/History/HistoryRepository.cs
@@ -29,16 +29,12 @@ namespace NzbDrone.Core.History
 
         public EntityHistory MostRecentForBook(int bookId)
         {
-            return Query(h => h.BookId == bookId)
-                .OrderByDescending(h => h.Date)
-                .FirstOrDefault();
+            return Query(h => h.BookId == bookId).MaxBy(h => h.Date);
         }
 
         public EntityHistory MostRecentForDownloadId(string downloadId)
         {
-            return Query(h => h.DownloadId == downloadId)
-                .OrderByDescending(h => h.Date)
-                .FirstOrDefault();
+            return Query(h => h.DownloadId == downloadId).MaxBy(h => h.Date);
         }
 
         public List<EntityHistory> FindByDownloadId(string downloadId)