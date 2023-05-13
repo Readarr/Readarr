Sonarr/Sonarr

Added searchEngine support in Newznab/Torznab caps

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -97,5 +97,28 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
 
             ExceptionVerification.ExpectedErrors(1);
         }
+
+        [Test]
+        public void should_use_default_searchengine_if_missing()
+        {
+            GivenCapsResponse(_caps);
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("sphinx");
+            caps.TvTextSearchEngine.Should().Be("sphinx");
+        }
+
+        [Test]
+        public void should_use_specified_searchengine()
+        {
+            GivenCapsResponse(_caps.Replace("<search ", "<search searchEngine=\"raw\" ")
+                                   .Replace("<tv-search ", "<tv-search searchEngine=\"raw2\" "));
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("raw");
+            caps.TvTextSearchEngine.Should().Be("raw2");
+        }
     }
 }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilities.cs
@@ -9,6 +9,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         public string[] SupportedSearchParameters { get; set; }
         public string[] SupportedTvSearchParameters { get; set; }
         public bool SupportsAggregateIdSearch { get; set; }
+        public string TextSearchEngine { get; set; }
+        public string TvTextSearchEngine { get; set; }
         public List<NewznabCategory> Categories { get; set; }
 
         public NewznabCapabilities()
@@ -18,6 +20,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             SupportedSearchParameters = new[] { "q" };
             SupportedTvSearchParameters = new[] { "q", "rid", "season", "ep" }; // This should remain 'rid' for older newznab installs.
             SupportsAggregateIdSearch = false;
+            TextSearchEngine = "sphinx";    // This should remain 'sphinx' for odler newznab installs
+            TvTextSearchEngine = "sphinx";  // This should remain 'sphinx' for odler newznab installs
             Categories = new List<NewznabCategory>();
         }
     }

Readarr/Readarr

(cherry picked from commit 1cbcad6960c035603e1dc3cf8ddb0c487a29969d)

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -97,5 +97,28 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
 
             ExceptionVerification.ExpectedErrors(1);
         }
+
+        [Test]
+        public void should_use_default_searchengine_if_missing()
+        {
+            GivenCapsResponse(_caps);
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("sphinx");
+            caps.BookTextSearchEngine.Should().Be("sphinx");
+        }
+
+        [Test]
+        public void should_use_specified_searchengine()
+        {
+            GivenCapsResponse(_caps.Replace("<search ", "<search searchEngine=\"raw\" ")
+                                   .Replace("<tv-search ", "<tv-search searchEngine=\"raw2\" "));
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("raw");
+            caps.BookTextSearchEngine.Should().Be("raw2");
+        }
     }
 }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilities.cs
@@ -7,9 +7,12 @@ namespace NzbDrone.Core.Indexers.Newznab
         public int DefaultPageSize { get; set; }
         public int MaxPageSize { get; set; }
         public string[] SupportedSearchParameters { get; set; }
-        public string[] SupportedTvSearchParameters { get; set; }
+        public string[] SupportedAudioSearchParameters { get; set; }
         public string[] SupportedBookSearchParameters { get; set; }
         public bool SupportsAggregateIdSearch { get; set; }
+        public string TextSearchEngine { get; set; }
+        public string AudioTextSearchEngine { get; set; }
+        public string BookTextSearchEngine { get; set; }
         public List<NewznabCategory> Categories { get; set; }
 
         public NewznabCapabilities()
@@ -17,9 +20,12 @@ namespace NzbDrone.Core.Indexers.Newznab
             DefaultPageSize = 100;
             MaxPageSize = 100;
             SupportedSearchParameters = new[] { "q" };
-            SupportedTvSearchParameters = new[] { "q", "rid", "season", "ep" }; // This should remain 'rid' for older newznab installs.
+            SupportedAudioSearchParameters = new[] { "q", "artist", "album" };
             SupportedBookSearchParameters = new[] { "q", "author", "title" };
             SupportsAggregateIdSearch = false;
+            TextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
+            AudioTextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
+            BookTextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
             Categories = new List<NewznabCategory>();
         }
     }