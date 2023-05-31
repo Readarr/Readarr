b05938a9a82d8269e0fa738e2bd94d1508bdfd6c
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-05-31 06:51:20 +0300

    Revert "Fixed: Don't log handled exceptions in API"
    This reverts commit fecb3895ed52c55391c931dfd9a5f1c119a75a85.

--- src/NzbDrone.Host/Bootstrap.cs
+++ src/NzbDrone.Host/Bootstrap.cs
@@ -13,7 +13,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.WindowsServices;
-using Microsoft.Extensions.Logging;
 using NLog;
 using Npgsql;
 using NzbDrone.Common.Composition.Extensions;
@@ -27,7 +26,6 @@ using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Datastore.Extensions;
 using NzbDrone.Core.Lifecycle;
 using NzbDrone.Core.Messaging.Events;
-using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 using PostgresOptions = NzbDrone.Core.Datastore.PostgresOptions;
 
 namespace NzbDrone.Host
@@ -150,10 +148,6 @@ namespace NzbDrone.Host
             return new HostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseServiceProviderFactory(new DryIocServiceProviderFactory(new Container(rules => rules.WithNzbDroneRules())))
-                .ConfigureLogging(logging =>
-                {
-                    logging.AddFilter("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogLevel.None);
-                })
                 .ConfigureContainer<IContainer>(c =>
                 {
                     c.AutoAddServices(Bootstrap.ASSEMBLIES)
