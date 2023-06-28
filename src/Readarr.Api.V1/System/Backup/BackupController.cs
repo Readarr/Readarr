dbcb0e77a8fa048580d2ddd188d3c6a98e449b3f
Qstick <qstick@gmail.com>
2023-06-27 22:05:37 -0500

    Fixed: Allow restore to process backups up to ~500MB
    (cherry picked from commit 551edb9e655d2a541a2232f85e79a5e3f7b433aa)

--- src/Readarr.Api.V1/System/Backup/BackupController.cs
+++ src/Readarr.Api.V1/System/Backup/BackupController.cs
@@ -90,6 +90,7 @@ namespace Readarr.Api.V1.System.Backup
         }
 
         [HttpPost("restore/upload")]
+        [RequestFormLimits(MultipartBodyLengthLimit = 500000000)]
         public object UploadAndRestore()
         {
             var files = Request.Form.Files;
