7d77b1fbe5f0e016a2848ca570d235b29bdcaab1
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-23 05:02:18 +0300

    Trim spaces from a split list in GetValueConverter

--- src/Readarr.Http/ClientSchema/SchemaBuilder.cs
+++ src/Readarr.Http/ClientSchema/SchemaBuilder.cs
@@ -241,7 +241,7 @@ namespace Readarr.Http.ClientSchema
                     }
                     else
                     {
-                        return fieldValue.ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        return fieldValue.ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(v => v.Trim());
                     }
                 };
             }
