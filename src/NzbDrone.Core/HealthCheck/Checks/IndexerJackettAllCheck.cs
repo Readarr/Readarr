b49d2312abc722a1345cecbe5ec62d6c3db6bc90
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-29 04:37:21 +0300

    Fixed: Check only enabled Jackett indexers for '/all' endpoint
    (cherry picked from commit ae3dd5730e05c5229e7f7092f15c33859524863b)
    
    Closes #2730

--- src/NzbDrone.Core/HealthCheck/Checks/IndexerJackettAllCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/IndexerJackettAllCheck.cs
@@ -8,6 +8,7 @@ using NzbDrone.Core.ThingiProvider.Events;
 
 namespace NzbDrone.Core.HealthCheck.Checks
 {
+    [CheckOn(typeof(ProviderAddedEvent<IIndexer>))]
     [CheckOn(typeof(ProviderUpdatedEvent<IIndexer>))]
     [CheckOn(typeof(ProviderDeletedEvent<IIndexer>))]
     [CheckOn(typeof(ProviderStatusChangedEvent<IIndexer>))]
@@ -23,12 +24,15 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
         public override HealthCheck Check()
         {
-            var jackettAllProviders = _providerFactory.All().Where(
-                i => i.ConfigContract.Equals("TorznabSettings") &&
-                ((i.Settings as TorznabSettings).BaseUrl.Contains("/torznab/all/api", StringComparison.InvariantCultureIgnoreCase) ||
-                (i.Settings as TorznabSettings).BaseUrl.Contains("/api/v2.0/indexers/all/results/torznab", StringComparison.InvariantCultureIgnoreCase) ||
-                (i.Settings as TorznabSettings).ApiPath.Contains("/torznab/all/api", StringComparison.InvariantCultureIgnoreCase) ||
-                (i.Settings as TorznabSettings).ApiPath.Contains("/api/v2.0/indexers/all/results/torznab", StringComparison.InvariantCultureIgnoreCase)));
+            var jackettAllProviders = _providerFactory.All()
+                .Where(
+                    i => i.Enable &&
+                         i.ConfigContract.Equals("TorznabSettings") &&
+                         (((TorznabSettings)i.Settings).BaseUrl.Contains("/torznab/all/api", StringComparison.InvariantCultureIgnoreCase) ||
+                          ((TorznabSettings)i.Settings).BaseUrl.Contains("/api/v2.0/indexers/all/results/torznab", StringComparison.InvariantCultureIgnoreCase) ||
+                          ((TorznabSettings)i.Settings).ApiPath.Contains("/torznab/all/api", StringComparison.InvariantCultureIgnoreCase) ||
+                          ((TorznabSettings)i.Settings).ApiPath.Contains("/api/v2.0/indexers/all/results/torznab", StringComparison.InvariantCultureIgnoreCase)))
+                .ToArray();
 
             if (jackettAllProviders.Empty())
             {
@@ -37,8 +41,7 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             return new HealthCheck(GetType(),
                 HealthCheckResult.Warning,
-                string.Format(_localizationService.GetLocalizedString("IndexerJackettAll"),
-                    string.Join(", ", jackettAllProviders.Select(i => i.Name))),
+                string.Format(_localizationService.GetLocalizedString("IndexerJackettAll"), string.Join(", ", jackettAllProviders.Select(i => i.Name))),
                 "#jackett-all-endpoint-used");
         }
     }
