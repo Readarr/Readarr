722da839841a82f99448b8bade6e3e3226503520
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-09-07 09:08:55 +0300

    New: Add Plex Media Server notifications

---
+++ src/NzbDrone.Core/Notifications/Plex/Server/PlexServerService.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FluentValidation.Results;
+using NLog;
+using NzbDrone.Common.Cache;
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Books;
+using NzbDrone.Core.RootFolders;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Notifications.Plex.Server
+{
+    public interface IPlexServerService
+    {
+        void UpdateLibrary(Author author, PlexServerSettings settings);
+        void UpdateLibrary(IEnumerable<Author> authors, PlexServerSettings settings);
+        ValidationFailure Test(PlexServerSettings settings);
+    }
+
+    public class PlexServerService : IPlexServerService
+    {
+        private readonly ICached<Version> _versionCache;
+        private readonly IPlexServerProxy _plexServerProxy;
+        private readonly IRootFolderService _rootFolderService;
+        private readonly Logger _logger;
+
+        public PlexServerService(ICacheManager cacheManager, IPlexServerProxy plexServerProxy, IRootFolderService rootFolderService, Logger logger)
+        {
+            _versionCache = cacheManager.GetCache<Version>(GetType(), "versionCache");
+            _plexServerProxy = plexServerProxy;
+            _rootFolderService = rootFolderService;
+            _logger = logger;
+        }
+
+        public void UpdateLibrary(Author author, PlexServerSettings settings)
+        {
+            UpdateLibrary(new[] { author }, settings);
+        }
+
+        public void UpdateLibrary(IEnumerable<Author> authors, PlexServerSettings settings)
+        {
+            try
+            {
+                _logger.Debug("Sending Update Request to Plex Server");
+                var watch = Stopwatch.StartNew();
+
+                var version = _versionCache.Get(settings.Host, () => GetVersion(settings), TimeSpan.FromHours(2));
+                ValidateVersion(version);
+
+                var sections = GetSections(settings);
+
+                foreach (var author in authors)
+                {
+                    UpdateSections(author, sections, settings);
+                }
+
+                _logger.Debug("Finished sending Update Request to Plex Server (took {0} ms)", watch.ElapsedMilliseconds);
+            }
+            catch (Exception ex)
+            {
+                _logger.Warn(ex, "Failed to Update Plex host: " + settings.Host);
+                throw;
+            }
+        }
+
+        private List<PlexSection> GetSections(PlexServerSettings settings)
+        {
+            _logger.Debug("Getting sections from Plex host: {0}", settings.Host);
+
+            return _plexServerProxy.GetTvSections(settings).ToList();
+        }
+
+        private void ValidateVersion(Version version)
+        {
+            if (version >= new Version(1, 3, 0) && version < new Version(1, 3, 1))
+            {
+                throw new PlexVersionException("Found version {0}, upgrade to PMS 1.3.1 to fix library updating and then restart Readarr", version);
+            }
+        }
+
+        private Version GetVersion(PlexServerSettings settings)
+        {
+            _logger.Debug("Getting version from Plex host: {0}", settings.Host);
+
+            var rawVersion = _plexServerProxy.Version(settings);
+            var version = new Version(Regex.Match(rawVersion, @"^(\d+[.-]){4}").Value.Trim('.', '-'));
+
+            return version;
+        }
+
+        private void UpdateSections(Author author, List<PlexSection> sections, PlexServerSettings settings)
+        {
+            var rootFolderPath = _rootFolderService.GetBestRootFolderPath(author.Path);
+            var authorRelativePath = rootFolderPath.GetRelativePath(author.Path);
+
+            // Try to update a matching section location before falling back to updating all section locations.
+            foreach (var section in sections)
+            {
+                foreach (var location in section.Locations)
+                {
+                    var rootFolder = new OsPath(rootFolderPath);
+                    var mappedPath = rootFolder;
+
+                    if (settings.MapTo.IsNotNullOrWhiteSpace())
+                    {
+                        mappedPath = new OsPath(settings.MapTo) + (rootFolder - new OsPath(settings.MapFrom));
+
+                        _logger.Trace("Mapping Path from {0} to {1} for partial scan", rootFolder, mappedPath);
+                    }
+
+                    if (location.Path.PathEquals(mappedPath.FullPath))
+                    {
+                        _logger.Debug("Updating matching section location, {0}", location.Path);
+                        UpdateSectionPath(authorRelativePath, section, location, settings);
+
+                        return;
+                    }
+                }
+            }
+
+            _logger.Debug("Unable to find matching section location, updating all Music sections");
+
+            foreach (var section in sections)
+            {
+                foreach (var location in section.Locations)
+                {
+                    UpdateSectionPath(authorRelativePath, section, location, settings);
+                }
+            }
+        }
+
+        private void UpdateSectionPath(string authorRelativePath, PlexSection section, PlexSectionLocation location, PlexServerSettings settings)
+        {
+            var separator = location.Path.Contains('\\') ? "\\" : "/";
+            var locationRelativePath = authorRelativePath.Replace("\\", separator).Replace("/", separator);
+
+            // Plex location paths trim trailing extraneous separator characters, so it doesn't need to be trimmed
+            var pathToUpdate = $"{location.Path}{separator}{locationRelativePath}";
+
+            _logger.Debug("Updating section location, {0}", location.Path);
+            _plexServerProxy.Update(section.Id, pathToUpdate, settings);
+        }
+
+        public ValidationFailure Test(PlexServerSettings settings)
+        {
+            try
+            {
+                _versionCache.Remove(settings.Host);
+                var sections = GetSections(settings);
+
+                if (sections.Empty())
+                {
+                    return new ValidationFailure("Host", "At least one Music library is required");
+                }
+            }
+            catch (PlexAuthenticationException ex)
+            {
+                _logger.Error(ex, "Unable to connect to Plex Media Server");
+                return new ValidationFailure("AuthToken", "Invalid authentication token");
+            }
+            catch (PlexException ex)
+            {
+                return new NzbDroneValidationFailure("Host", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to connect to Plex Media Server");
+
+                return new NzbDroneValidationFailure("Host", "Unable to connect to Plex Media Server")
+                       {
+                           DetailedDescription = ex.Message
+                       };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Notifications/Plex/Server/PlexServerSettings.cs
@@ -0,0 +1,62 @@
+using FluentValidation;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Notifications.Plex.Server
+{
+    public class PlexServerSettingsValidator : AbstractValidator<PlexServerSettings>
+    {
+        public PlexServerSettingsValidator()
+        {
+            RuleFor(c => c.Host).ValidHost();
+            RuleFor(c => c.Port).InclusiveBetween(1, 65535);
+            RuleFor(c => c.MapFrom).NotEmpty().Unless(c => c.MapTo.IsNullOrWhiteSpace());
+            RuleFor(c => c.MapTo).NotEmpty().Unless(c => c.MapFrom.IsNullOrWhiteSpace());
+        }
+    }
+
+    public class PlexServerSettings : IProviderConfig
+    {
+        private static readonly PlexServerSettingsValidator Validator = new PlexServerSettingsValidator();
+
+        public PlexServerSettings()
+        {
+            Port = 32400;
+            UpdateLibrary = true;
+            SignIn = "startOAuth";
+        }
+
+        [FieldDefinition(0, Label = "Host")]
+        public string Host { get; set; }
+
+        [FieldDefinition(1, Label = "Port")]
+        public int Port { get; set; }
+
+        [FieldDefinition(2, Label = "Use SSL", Type = FieldType.Checkbox, HelpText = "Connect to Plex over HTTPS instead of HTTP")]
+        public bool UseSsl { get; set; }
+
+        [FieldDefinition(3, Label = "Auth Token", Type = FieldType.Textbox, Privacy = PrivacyLevel.ApiKey, Advanced = true)]
+        public string AuthToken { get; set; }
+
+        [FieldDefinition(4, Label = "Authenticate with Plex.tv", Type = FieldType.OAuth)]
+        public string SignIn { get; set; }
+
+        [FieldDefinition(5, Label = "Update Library", Type = FieldType.Checkbox)]
+        public bool UpdateLibrary { get; set; }
+
+        [FieldDefinition(6, Label = "Map Paths From", Type = FieldType.Textbox, Advanced = true, HelpText = "Readarr path, used to modify author paths when Plex sees library path location differently from Readarr")]
+        public string MapFrom { get; set; }
+
+        [FieldDefinition(7, Label = "Map Paths To", Type = FieldType.Textbox, Advanced = true, HelpText = "Plex path, used to modify author paths when Plex sees library path location differently from Readarr")]
+        public string MapTo { get; set; }
+
+        public bool IsValid => !string.IsNullOrWhiteSpace(Host);
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
