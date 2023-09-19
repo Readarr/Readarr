b38d09448d7d0475010b5cf5303438539a083cea
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-09-19 07:05:28 +0300

    Fixed: Skip parsing releases without title
    (cherry picked from commit c7824bb593291634bf14a5f7aa689666969b03bf)

--- src/NzbDrone.Core/Parser/QualityParser.cs
+++ src/NzbDrone.Core/Parser/QualityParser.cs
@@ -31,7 +31,12 @@ namespace NzbDrone.Core.Parser
 
         public static QualityModel ParseQuality(string name, string desc = null, List<int> categories = null)
         {
-            Logger.Debug("Trying to parse quality for {0}", name);
+            Logger.Debug("Trying to parse quality for '{0}'", name);
+
+            if (name.IsNullOrWhiteSpace())
+            {
+                return new QualityModel { Quality = Quality.Unknown };
+            }
 
             var normalizedName = name.Replace('_', ' ').Trim().ToLower();
             var result = ParseQualityModifiers(name, normalizedName);
