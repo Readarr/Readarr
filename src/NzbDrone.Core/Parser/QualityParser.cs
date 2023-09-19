1a30293c33c2fa89c8bd33a94fe3b10c732f489e
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-09-19 21:33:26 +0300

    Check for empty description as well in ParseQuality

--- src/NzbDrone.Core/Parser/QualityParser.cs
+++ src/NzbDrone.Core/Parser/QualityParser.cs
@@ -33,7 +33,7 @@ namespace NzbDrone.Core.Parser
         {
             Logger.Debug("Trying to parse quality for '{0}'", name);
 
-            if (name.IsNullOrWhiteSpace())
+            if (name.IsNullOrWhiteSpace() && desc.IsNullOrWhiteSpace())
             {
                 return new QualityModel { Quality = Quality.Unknown };
             }
