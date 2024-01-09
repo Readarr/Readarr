Sonarr/Sonarr

New: Download client option for redownloading failed releases from Interactive Search
Closes #5580

src/NzbDrone.Core/Download/RedownloadFailedDownloadService.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.IndexerSearch;
 using NzbDrone.Core.Messaging;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Download
@@ -39,6 +40,12 @@ namespace NzbDrone.Core.Download
             if (!_configService.AutoRedownloadFailed)
             {
                 _logger.Debug("Auto redownloading failed episodes is disabled");
+                return;
+            }
+
+            if (message.ReleaseSource == ReleaseSourceType.InteractiveSearch && !_configService.AutoRedownloadFailedFromInteractiveSearch)
+            {
+                _logger.Debug("Auto redownloading failed episodes from interactive search is disabled");
                 return;
             }
 

Readarr/Readarr

(cherry picked from commit 87e0a7983a437a4d166aa8b9c9eaf78ea5431969)
Closes #2987

src/NzbDrone.Core/Download/RedownloadFailedDownloadService.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.IndexerSearch;
 using NzbDrone.Core.Messaging;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download
 {
@@ -38,6 +39,12 @@ namespace NzbDrone.Core.Download
             if (!_configService.AutoRedownloadFailed)
             {
                 _logger.Debug("Auto redownloading failed books is disabled");
+                return;
+            }
+
+            if (message.ReleaseSource == ReleaseSourceType.InteractiveSearch && !_configService.AutoRedownloadFailedFromInteractiveSearch)
+            {
+                _logger.Debug("Auto redownloading failed albumbs from interactive search is disabled");
                 return;
             }
 