ZephireNZ/Sonarr

Fixed: Blocklisting torrents from indexers that do not provide torrent hash
Closes #6108

src/NzbDrone.Core/Download/DownloadService.cs
@@ -104,6 +104,11 @@ namespace NzbDrone.Core.Download
                 _logger.Trace("Release {0} no longer available on indexer.", remoteEpisode);
                 throw;
             }
+            catch (ReleaseBlockedException)
+            {
+                _logger.Trace("Release {0} previously added to blocklist, not sending to download client again.", remoteEpisode);
+                throw;
+            }
             catch (DownloadClientRejectedReleaseException)
             {
                 _logger.Trace("Release {0} rejected by download client, possible duplicate.", remoteEpisode);
@@ -128,7 +133,7 @@ namespace NzbDrone.Core.Download
             episodeGrabbedEvent.DownloadClientId = downloadClient.Definition.Id;
             episodeGrabbedEvent.DownloadClientName = downloadClient.Definition.Name;
 
-            if (!string.IsNullOrWhiteSpace(downloadClientId))
+            if (downloadClientId.IsNotNullOrWhiteSpace())
             {
                 episodeGrabbedEvent.DownloadId = downloadClientId;
             }

Readarr/Readarr

(cherry picked from commit 3541cd7ba877fb785c7f97123745abf51162eb8e)
Closes #3082

src/NzbDrone.Core/Download/DownloadService.cs
@@ -102,6 +102,11 @@ namespace NzbDrone.Core.Download
                 _logger.Trace("Release {0} no longer available on indexer.", remoteBook);
                 throw;
             }
+            catch (ReleaseBlockedException)
+            {
+                _logger.Trace("Release {0} previously added to blocklist, not sending to download client again.", remoteBook);
+                throw;
+            }
             catch (DownloadClientRejectedReleaseException)
             {
                 _logger.Trace("Release {0} rejected by download client, possible duplicate.", remoteBook);
@@ -126,7 +131,7 @@ namespace NzbDrone.Core.Download
             bookGrabbedEvent.DownloadClientId = downloadClient.Definition.Id;
             bookGrabbedEvent.DownloadClientName = downloadClient.Definition.Name;
 
-            if (!string.IsNullOrWhiteSpace(downloadClientId))
+            if (downloadClientId.IsNotNullOrWhiteSpace())
             {
                 bookGrabbedEvent.DownloadId = downloadClientId;
             }