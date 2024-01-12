Sonarr/Sonarr

Paging params in API docs
Closes #6003

src/NzbDrone.Integration.Test/ApiTests/WantedTests/MissingFixture.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
         {
             EnsureSeries(266189, "The Blacklist", false);
 
-            var result = WantedMissing.GetPaged(0, 15, "airDateUtc", "desc", "monitored", "false");
+            var result = WantedMissing.GetPaged(0, 15, "airDateUtc", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }

src/Sonarr.Api.V3/Queue/QueueController.cs
@@ -135,9 +135,9 @@ namespace Sonarr.Api.V3.Queue
 
         [HttpGet]
         [Produces("application/json")]
-        public PagingResource<QueueResource> GetQueue(bool includeUnknownSeriesItems = false, bool includeSeries = false, bool includeEpisode = false)
+        public PagingResource<QueueResource> GetQueue([FromQuery] PagingRequestResource paging, bool includeUnknownSeriesItems = false, bool includeSeries = false, bool includeEpisode = false)
         {
-            var pagingResource = Request.ReadPagingResourceFromRequest<QueueResource>();
+            var pagingResource = new PagingResource<QueueResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<QueueResource, NzbDrone.Core.Queue.Queue>("timeleft", SortDirection.Ascending);
 
             return pagingSpec.ApplyToPage((spec) => GetQueue(spec, includeUnknownSeriesItems), (q) => MapToResource(q, includeSeries, includeEpisode));

Readarr/Readarr

(cherry picked from commit bfaa7291e14a8d3847ef2154a52c363944560803)
Closes #2975
Closes #2991

src/NzbDrone.Integration.Test/ApiTests/WantedTests/MissingFixture.cs
@@ -86,7 +86,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
         {
             EnsureAuthor("14586394", "43765115", "Andrew Hunter Murray", false);
 
-            var result = WantedMissing.GetPaged(0, 15, "releaseDate", "desc", "monitored", "false");
+            var result = WantedMissing.GetPaged(0, 15, "releaseDate", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }

src/Readarr.Api.V1/Queue/QueueController.cs
@@ -100,9 +100,10 @@ namespace Readarr.Api.V1.Queue
         }
 
         [HttpGet]
-        public PagingResource<QueueResource> GetQueue(bool includeUnknownAuthorItems = false, bool includeAuthor = false, bool includeBook = false)
+        [Produces("application/json")]
+        public PagingResource<QueueResource> GetQueue([FromQuery] PagingRequestResource paging, bool includeUnknownAuthorItems = false, bool includeAuthor = false, bool includeBook = false)
         {
-            var pagingResource = Request.ReadPagingResourceFromRequest<QueueResource>();
+            var pagingResource = new PagingResource<QueueResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<QueueResource, NzbDrone.Core.Queue.Queue>("timeleft", SortDirection.Ascending);
 
             return pagingSpec.ApplyToPage((spec) => GetQueue(spec, includeUnknownAuthorItems), (q) => MapToResource(q, includeAuthor, includeBook));