Sonarr/Sonarr

New: Add FileId to History data for import events

src/NzbDrone.Core/History/HistoryService.cs
@@ -211,8 +211,7 @@ namespace NzbDrone.Core.History
                         Language = message.EpisodeInfo.Language
                     };
 
-                //Won't have a value since we publish this event before saving to DB.
-                //history.Data.Add("FileId", message.ImportedEpisode.Id.ToString());
+                history.Data.Add("FileId", message.ImportedEpisode.Id.ToString());
                 history.Data.Add("DroppedPath", message.EpisodeInfo.Path);
                 history.Data.Add("ImportedPath", Path.Combine(message.EpisodeInfo.Series.Path, message.ImportedEpisode.RelativePath));
                 history.Data.Add("DownloadClient", message.DownloadClientInfo?.Type);

Readarr/Readarr

(cherry picked from commit 952a7248c962908fc5da92762507421923a06e17)
Closes #788

src/NzbDrone.Core/History/HistoryService.cs
@@ -230,8 +230,7 @@ namespace NzbDrone.Core.History
                 DownloadId = downloadId
             };
 
-            //Won't have a value since we publish this event before saving to DB.
-            //history.Data.Add("FileId", message.ImportedEpisode.Id.ToString());
+            history.Data.Add("FileId", message.ImportedBook.Id.ToString());
             history.Data.Add("DroppedPath", message.BookInfo.Path);
             history.Data.Add("ImportedPath", message.ImportedBook.Path);
             history.Data.Add("DownloadClient", message.DownloadClientInfo?.Type);