Sonarr/Sonarr

New: Indexer flags
Closes #2782

src/NzbDrone.Core/CustomFormats/Specifications/IndexerFlagSpecification.cs
@@ -1,0 +1,44 @@
+using System;
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.CustomFormats
+{
+    public class IndexerFlagSpecificationValidator : AbstractValidator<IndexerFlagSpecification>
+    {
+        public IndexerFlagSpecificationValidator()
+        {
+            RuleFor(c => c.Value).NotEmpty();
+            RuleFor(c => c.Value).Custom((qualityValue, context) =>
+            {
+                if (!Enum.IsDefined(typeof(IndexerFlags), qualityValue))
+                {
+                    context.AddFailure($"Invalid indexer flag condition value: {qualityValue}");
+                }
+            });
+        }
+    }
+
+    public class IndexerFlagSpecification : CustomFormatSpecificationBase
+    {
+        private static readonly IndexerFlagSpecificationValidator Validator = new ();
+
+        public override int Order => 4;
+        public override string ImplementationName => "Indexer Flag";
+
+        [FieldDefinition(1, Label = "CustomFormatsSpecificationFlag", Type = FieldType.Select, SelectOptions = typeof(IndexerFlags))]
+        public int Value { get; set; }
+
+        protected override bool IsSatisfiedByWithoutNegate(CustomFormatInput input)
+        {
+            return input.IndexerFlags.HasFlag((IndexerFlags)Value);
+        }
+
+        public override NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}

src/Sonarr.Api.V3/Indexers/IndexerFlagController.cs
@@ -1,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Parser.Model;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V3.Indexers
+{
+    [V3ApiController]
+    public class IndexerFlagController : Controller
+    {
+        [HttpGet]
+        public List<IndexerFlagResource> GetAll()
+        {
+            return Enum.GetValues(typeof(IndexerFlags)).Cast<IndexerFlags>().Select(f => new IndexerFlagResource
+            {
+                Id = (int)f,
+                Name = f.ToString()
+            }).ToList();
+        }
+    }
+}

Readarr/Readarr

(cherry picked from commit 7a768b5d0faf9aa57e78aee19cefee8fb19a42d5)

src/NzbDrone.Core/CustomFormats/Specifications/IndexerFlagSpecification.cs
@@ -1,0 +1,44 @@
+using System;
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.CustomFormats
+{
+    public class IndexerFlagSpecificationValidator : AbstractValidator<IndexerFlagSpecification>
+    {
+        public IndexerFlagSpecificationValidator()
+        {
+            RuleFor(c => c.Value).NotEmpty();
+            RuleFor(c => c.Value).Custom((flag, context) =>
+            {
+                if (!Enum.IsDefined(typeof(IndexerFlags), flag))
+                {
+                    context.AddFailure($"Invalid indexer flag condition value: {flag}");
+                }
+            });
+        }
+    }
+
+    public class IndexerFlagSpecification : CustomFormatSpecificationBase
+    {
+        private static readonly IndexerFlagSpecificationValidator Validator = new ();
+
+        public override int Order => 4;
+        public override string ImplementationName => "Indexer Flag";
+
+        [FieldDefinition(1, Label = "CustomFormatsSpecificationFlag", Type = FieldType.Select, SelectOptions = typeof(IndexerFlags))]
+        public int Value { get; set; }
+
+        protected override bool IsSatisfiedByWithoutNegate(CustomFormatInput input)
+        {
+            return input.IndexerFlags.HasFlag((IndexerFlags)Value);
+        }
+
+        public override NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}

src/Readarr.Api.V1/Indexers/IndexerFlagController.cs
@@ -1,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Parser.Model;
+using Readarr.Http;
+
+namespace Readarr.Api.V1.Indexers
+{
+    [V1ApiController]
+    public class IndexerFlagController : Controller
+    {
+        [HttpGet]
+        public List<IndexerFlagResource> GetAll()
+        {
+            return Enum.GetValues(typeof(IndexerFlags)).Cast<IndexerFlags>().Select(f => new IndexerFlagResource
+            {
+                Id = (int)f,
+                Name = f.ToString()
+            }).ToList();
+        }
+    }
+}