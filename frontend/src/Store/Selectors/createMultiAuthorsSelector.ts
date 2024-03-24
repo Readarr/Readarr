4fb62c072a4ec872a9b1e24741ab7bb331313dc4
Mark McDowall <mark@mcdowall.ca>
2024-03-23 21:42:54 -0700

    Fixed: Task with removed author causing error
    (cherry picked from commit fc6494c569324c839debdb1d08dde23b8f1b8d76)
    
    Closes #3376

--- frontend/src/Store/Selectors/createMultiAuthorsSelector.ts
+++ frontend/src/Store/Selectors/createMultiAuthorsSelector.ts
@@ -1,12 +1,21 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
+import Author from 'Author/Author';
 
 function createMultiAuthorsSelector(authorIds: number[]) {
   return createSelector(
     (state: AppState) => state.authors.itemMap,
     (state: AppState) => state.authors.items,
     (itemMap, allAuthors) => {
-      return authorIds.map((authorId) => allAuthors[itemMap[authorId]]);
+      return authorIds.reduce((acc: Author[], authorId) => {
+        const author = allAuthors[itemMap[authorId]];
+
+        if (author) {
+          acc.push(author);
+        }
+
+        return acc;
+      }, []);
     }
   );
 }
