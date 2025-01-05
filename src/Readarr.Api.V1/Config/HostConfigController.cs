6af3d64b4bd2498e2c6c15c85b4cd90adde62367
Stevie Robinson <stevie.robinson@gmail.com>
2025-01-05 02:58:24 +0100

    Fixed: Listening on all IPv4 Addresses
    (cherry picked from commit 035c474f10c257331a5f47e863d24af82537e335)

--- src/Readarr.Api.V1/Config/HostConfigController.cs
+++ src/Readarr.Api.V1/Config/HostConfigController.cs
@@ -34,7 +34,6 @@ namespace Readarr.Api.V1.Config
 
             SharedValidator.RuleFor(c => c.BindAddress)
                            .ValidIpAddress()
-                           .NotListenAllIp4Address()
                            .When(c => c.BindAddress != "*" && c.BindAddress != "localhost");
 
             SharedValidator.RuleFor(c => c.Port).ValidPort();
