a4290df00079bc9ee57b3cd136ec27f053141a9b
Kyle Butler <overclock_mobo@hotmail.com>
2025-02-24 11:18:32 -0500

    fixup! Adding missing series Deleted UI elements

--- frontend/src/Author/AuthorStatus.ts
+++ frontend/src/Author/AuthorStatus.ts
@@ -1,25 +1,31 @@
+import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
 import { icons } from 'Helpers/Props';
 import translate from 'Utilities/String/translate';
 
-export function getAuthorStatusDetails(status) {
+interface AuthorStatus {
+  icon: IconDefinition;
+  title: string;
+  message: string;
+}
 
+export function getAuthorStatusDetails(status): AuthorStatus {
   let statusDetails = {
     icon: icons.AUTHOR_CONTINUING,
     title: translate('StatusEndedContinuing'),
-    message: translate('ContinuingMoreBooksAreExpected')
+    message: translate('ContinuingMoreBooksAreExpected'),
   };
 
   if (status === 'deleted') {
     statusDetails = {
       icon: icons.AUTHOR_DELETED,
       title: translate('StatusEndedDeceased'),
-      message: translate('NotContinuingAuthorDeceased')
+      message: translate('NotContinuingAuthorDeceased'),
     };
   } else if (status === 'ended') {
     statusDetails = {
       icon: icons.AUTHOR_ENDED,
       title: translate('StatusEndedEnded'),
-      message: translate('ContinuingNoAdditionalBooksAreExpected')
+      message: translate('ContinuingNoAdditionalBooksAreExpected'),
     };
   }
 
